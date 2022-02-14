30eee448c0a517224709a0d47025afb77d51b33d
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2022-02-14 12:58:29 -0800

    Provide placeholder Grommet theme to _app
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { withStyles } from '@material-ui/core';
+import { Grommet } from 'grommet';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme = {
+    global: {
+      font: {
+        family: 'Roboto',
+        size: '14px',
+        height: '20px',
+      },
+    },
+  };
+
+  return (
+    <Grommet theme={theme}>
+      <Component {...pageProps} />
+    </Grommet>
+  )
 }
 
 export default MyApp
