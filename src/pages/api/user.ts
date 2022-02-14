723659f668f405a62c62816e672ca6e5b639c60a
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-14 15:59:18 -0500

    User route is protected by SSR - Need to enable Amplify SSR in each route NOTE: Amplify is not supported for Next.js 12 middleware, so we have to protect - user_controller responds with username

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,20 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import user_controller from '../../backend/controller/user/user';
 
-type Data = {
-  name: string;
-};
+import Amplify, { withSSRContext } from 'aws-amplify';
+import awsExports from '../../aws-exports.js';
 
-export default function handler(
+// Amplify SSR configuration needs to be enabled within each API route
+Amplify.configure({ ...awsExports, ssr: true });
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  if (req.method === 'GET') {
-    // go to the get routes file import it from routes/get.ts
-    //Basic ping/pong endpoint to test server functionality
-    user_controller(req, res);
-  } else {
-    console.log(req.body);
-    // Handle any other HTTP method
+  const { Auth } = withSSRContext({ req });
+  try {
+    const user = await Auth.currentAuthenticatedUser();
+    if (req.method === 'GET') {
+      // go to the get routes file import it from routes/get.ts
+      //Basic ping/pong endpoint to test server functionality
+      user_controller(req, res, user);
+    } else {
+      console.log(req.body);
+      // Handle any other HTTP method
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ error: error });
   }
 }
