9d7e0dfe472592815a98f3f6a122e537fb8e6971
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-14 13:10:55 -0500

    Get username in profile page post signup/login

--- src/pages/app/profile/index.tsx
+++ src/pages/app/profile/index.tsx
@@ -1,11 +1,46 @@
 import type { NextPage } from 'next';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
+
+import { Amplify } from 'aws-amplify';
+import { Auth } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from '../../../aws-exports';
+Amplify.configure(awsExports);
+
 import { Box } from '@mui/material';
+
 import styles from '../../../styles/Signup.module.css';
 import Footer from '../../../frontend/components/footer';
 import Navbar from '../../../frontend/components/navbar';
 
 const MyProfile: NextPage = () => {
+  const [user, setUser] = useState(null);
+
+  const signOut = async function () {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log('error signing out: ', error);
+    }
+  };
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const user = await Auth.currentAuthenticatedUser();
+        const username = user.attributes.given_name;
+        console.log('User: ', user.attributes.given_name);
+        setUser(username);
+      } catch (error) {
+        console.log('error getting user: ', error);
+      }
+    };
+    getUser();
+  }, []);
+
   return (
     <>
       <Head>
@@ -15,10 +50,12 @@ const MyProfile: NextPage = () => {
       </Head>
 
       <Navbar />
-
       <Box className={styles.signup_wrapper}>
         <h1>MyProfile</h1>
+        <h1>Hello {user}</h1>
+        <button onClick={signOut}>Sign out</button>
       </Box>
+
       <Footer />
     </>
   );
