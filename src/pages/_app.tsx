02788cc1298c80700668625d9d124c6768aeb3a7
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-14 13:15:06 -0500

    Adding <NavBar/> component to root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,17 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Amplify from 'aws-amplify';
 import awsconfig from '../aws-exports';
+import Navbar from '@frontend/components/navbar';
 
 Amplify.configure({ ...awsconfig, ssr: true });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
