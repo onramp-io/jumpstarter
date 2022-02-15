78d2efbbb95b237310f3a525bab07ae1ea040258
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2022-02-15 09:25:10 -0800

    Fix margins on LandingComponent
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- src/frontend/components/landingComponent.tsx
+++ src/frontend/components/landingComponent.tsx
@@ -31,13 +31,21 @@ const LandingComponent = function landingComponentComponent<landingComponentProp
       align="center"
       margin="large"
     >
-      <Box height="medium" width="large">
+      <Box
+        height="medium"
+        width="large"
+        margin={{
+          bottom: "xsmall"
+        }}
+      >
         <Image fit="cover" src="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" />
       </Box>
-      <Heading margin="small" fill={true} level={1}>
+      <Heading margin="medium" fill={true} level={1}>
         Welcome to JumpStarter!
       </Heading>
-      <Main>
+      <Main margin={{
+        bottom: "medium"
+      }}>
         JumpStarter is a platform for people to jumpstart their projects, way
         better than kicking. Join today!
       </Main>
