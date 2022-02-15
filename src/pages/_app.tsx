cdaa1bd8bf52042b9966d571ff9485814c8fa4ea
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-15 15:20:00 -0500

    Update: 1. Wrap Layout to root component (@marri-kang update) 2. Removing amplify config code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,13 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Amplify from 'aws-amplify';
-import awsconfig from '../aws-exports';
-import Navbar from '@frontend/components/navbar';
-
-Amplify.configure({ ...awsconfig, ssr: true });
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Layout from '@frontend/components/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />;
-    </>
-  );
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp;
