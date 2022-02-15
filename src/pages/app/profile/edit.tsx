bf538f8cabf430fe812de59bd111a51bc5e7de62
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-15 15:25:44 -0500

    Update: 1. Remove <Head/>, <Footer/>(@marri-kang update) 2. Removing imports 3. Replace HTML tags with Grommet Box, Heading

--- src/pages/app/profile/edit.tsx
+++ src/pages/app/profile/edit.tsx
@@ -1,23 +1,13 @@
+import { Box, Heading } from 'grommet';
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import { Box } from '@mui/material';
 import styles from '../../../styles/Signup.module.css';
-import Footer from '../../../frontend/components/footer';
-import Navbar from '../../../frontend/components/navbar';
 
 const EditProfile: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>JumpStarter - EditProfile</title>
-        <meta name="description" content="Lets JumpStart projects" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <Box className={styles.signup_wrapper}>
-        <h1>EditProfile</h1>
+        <Heading>EditProfile</Heading>
       </Box>
-      <Footer />
     </>
   );
 };
