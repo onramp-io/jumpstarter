56c71d26a10ae99a9cec6f3e5210a055e34d9c5b
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2022-02-15 09:17:07 -0800

    Add Pagination to Section Cards
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- src/frontend/components/sectionMarquee.tsx
+++ src/frontend/components/sectionMarquee.tsx
@@ -2,7 +2,8 @@ import { NextPageContext } from 'next';
 import axios from 'axios';
 import SectionCard from './sectionCard';
 import styled from 'styled-components';
-import { Box } from 'grommet';
+import { Anchor, Box, Pagination } from 'grommet';
+import Link from 'next/link';
 
 interface ISingleAPIPayload {
   projectTitle: string,
@@ -18,26 +19,60 @@ const SectionMarquee = function sectionMarqueeComponent<sectionMarqueeProps>({ A
   /**
   * first 4 JSON payload from our API
   */
+  const handlePageChange = () => {
+    console.log("Changed page numbers!");
+  }
+
+  let numberOfPages = 40;
 
   return (
-    <Box
-      margin={{
-        left: "large",
-        right: "large"
-      }}
-      gap="medium"
-      direction="row"
-    >
-      {APIPayload.map(({
-        projectTitle,
-        projectDescription,
-        projectCreator,
-      }, i) => {
-        return (
-          < SectionCard key={i} projectTitle={projectTitle} projectDescription={projectDescription} projectCreator={projectCreator} />
-        )
-      })}
-    </Box>
+    <>
+      <Box>
+        <Box
+          align="end"
+          gap="small"
+          margin={{
+            bottom: "small",
+            right: "large"
+          }}
+        >
+          <Pagination onChange={handlePageChange} numberItems={numberOfPages} />
+        </Box>
+        <Box
+          margin={{
+            left: "large",
+            right: "large"
+          }}
+          gap="medium"
+          direction="row"
+        >
+          {APIPayload.map(({
+            projectTitle,
+            projectDescription,
+            projectCreator,
+          }, i) => {
+            return (
+              < SectionCard key={i} projectTitle={projectTitle} projectDescription={projectDescription} projectCreator={projectCreator} />
+            )
+          })}
+        </Box>
+        <Box direction="row" justify="end" margin={{
+          top: "medium",
+          right: "large"
+        }}>
+          <Link href="/project">
+            <Anchor
+              href="/project"
+              /** 
+               * 
+               icon={<GrommetIcon color="brand" />}
+               */
+              label="Create a New Project"
+            />
+          </Link>
+        </Box>
+      </Box>
+    </>
   );
 }
 
