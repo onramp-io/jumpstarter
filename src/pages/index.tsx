9de429221cef0a938188a1a10f841e2eda380585
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2022-02-14 23:56:45 -0800

    Refactor HomePage Component
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,69 @@
-import LandingComponent from '@frontend/landingComponent';
+import Section from '@frontend/components/sectionHeader';
+import LandingComponent from '@frontend/components/landingComponent';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Footer from '../frontend/components/footer';
 import Navbar from '../frontend/components/navbar';
 import styles from '../styles/Home.module.css';
+import SectionCard from '@frontend/components/sectionCard';
 
-const Home: NextPage = () => {
+import styled from 'styled-components';
+
+import { NextPageContext } from 'next';
+import SectionMarquee from '@frontend/components/sectionMarquee';
+import { Box } from 'grommet';
+
+interface indexProps {
+
+}
+
+const index: NextPage = function indexComponent<indexProps>({ }) {
+
+  const personalPicks = [
+    {
+      projectTitle: 'Personal Picks Project 1',
+      projectDescription: 'A brief description of what this project is. A second line for good measure.',
+      projectCreator: 'Example Creator 1',
+    },
+    {
+      projectTitle: 'Personal Picks Project 2',
+      projectDescription: 'A brief description of what this project is. A second line for good measure.',
+      projectCreator: 'Example Creator 2',
+    },
+    {
+      projectTitle: 'Personal Picks Project 3',
+      projectDescription: 'A brief description of what this project is. A second line for good measure.',
+      projectCreator: 'Example Creator 3',
+    },
+    {
+      projectTitle: 'Personal Picks Project 4',
+      projectDescription: 'A brief description of what this project is. A second line for good measure.',
+      projectCreator: 'Example Creator 4',
+    },
+  ];
+
+  const trendingProjects = [
+    {
+      projectTitle: 'Trending Project 1',
+      projectDescription: 'A brief description of what this project is. A second line for good measure.',
+      projectCreator: 'Example Creator 1',
+    },
+    {
+      projectTitle: 'Trending Project 2',
+      projectDescription: 'A brief description of what this project is. A second line for good measure.',
+      projectCreator: 'Example Creator 2',
+    },
+    {
+      projectTitle: 'Trending Project 3',
+      projectDescription: 'A brief description of what this project is. A second line for good measure.',
+      projectCreator: 'Example Creator 3',
+    },
+    {
+      projectTitle: 'Trending Project 4',
+      projectDescription: 'A brief description of what this project is. A second line for good measure.',
+      projectCreator: 'Example Creator 4',
+    },
+  ];
   return (
     <>
       <Head>
@@ -14,8 +72,40 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <LandingComponent />
+      <Box
+        align="center"
+        direction="column"
+        margin="large"
+      >
+        <Section sectionHeader="Personal Picks" sectionDescription="Here are some projects we think you'll love" />
+      </Box>
+      <Box
+        align="center"
+        direction="row"
+        margin="small"
+      >
+        <SectionMarquee APIPayload={personalPicks} />
+      </Box>
+      <Box
+        align="center"
+        direction="column"
+        margin="large"
+      >
+        <Section sectionHeader="Trending Projects" sectionDescription="See what's popular now" />
+      </Box>
+      <Box
+        align="center"
+        direction="row"
+        margin="small"
+      >
+        <SectionMarquee APIPayload={trendingProjects} />
+      </Box>
     </>
   );
+}
+
+index.getInitialProps = async ({ req }: NextPageContext) => {
+  return {};
 };
 
-export default Home;
+export default index;
