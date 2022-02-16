40b810ac915493133f97bca2e40d923fb1b3abf6
Marri Kang <mkang@bl-mkang-w3704q2tjn.lan>
2022-02-16 14:12:05 -0500

    Fix merge issue with aws-exports file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Layout from '@frontend/components/layout'
-import { Grommet } from 'grommet';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Layout from '@frontend/components/layout';
+import Amplify from 'aws-amplify';
+import awsconfig from '../aws-exports'
+
+Amplify.configure({ ...awsconfig, ssr: true });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Grommet plain>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Grommet>
-  )
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
