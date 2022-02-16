a0c2a6744d62a0ab1ae70feb090e6c4974c62a52
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2022-02-15 17:00:16 -0800

    Add DATABASE_URL to Next config
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     COGNITO_POOL_ID: process.env.COGNITO_POOL_ID,
     COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
+    DATABASE_URL: process.env.DATABASE_URL,
   },
   images: {
     domains: ['images.unsplash.com']
