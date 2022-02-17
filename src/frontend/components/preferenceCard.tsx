8a8a38fcd13421ff9070b4dbd32cbac3f8ccb0ee
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2022-02-17 15:40:01 -0800

    Remove unused tags and comments
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- src/frontend/components/preferenceCard.tsx
+++ src/frontend/components/preferenceCard.tsx
@@ -2,7 +2,7 @@ import { NextPageContext } from "next";
 import axios from "axios";
 import { motion } from "framer-motion";
 import { Box, Heading, Image, ResponsiveContext } from "grommet";
-import { Animations, hoverAnimation } from "utils/animations/motionObjects";
+import Animations from "utils/animations/motionObjects";
 
 export enum ProjectCategory {
   CATEGORY_1 = "Arts",
@@ -22,8 +22,15 @@ const PreferenceCard = function preferenceCardComponent<PreferenceCardProps>({
   imageUrl,
   projectCategory,
 }) {
+  const handleSelectCategory = (event) => {
+    console.log(event);
+  };
+
   return (
-    <motion.div whileHover={Animations.scaleOnHover}>
+    <motion.div
+      whileHover={Animations.scaleOnHover}
+      onClick={handleSelectCategory}
+    >
       <Box
         elevation="medium"
         margin={{
@@ -37,10 +44,6 @@ const PreferenceCard = function preferenceCardComponent<PreferenceCardProps>({
         align="center"
       >
         <Box flex={false} width="min(small, 100rem)" height="small">
-          {/** 
-       https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80
-       * 
-      */}
           <Image fit="cover" src={imageUrl} />
         </Box>
         <Heading className="no-text-wrap" level={2} margin="medium">
