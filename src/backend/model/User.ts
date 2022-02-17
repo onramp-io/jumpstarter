7c0c85a0d8817baa1fcaf98680472a7336908e21
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-17 14:23:06 -0500

    User Model (pmaganty update)

---
+++ src/backend/model/User.ts
@@ -0,0 +1,41 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Comment } from './Comment';
+import { Project } from './Project';
+import { Interest } from './Interest';
+import { Investment } from './Investment';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  first_name: string;
+
+  @Column('text')
+  last_name: string;
+
+  @Column('text')
+  email: string;
+
+  @Column('text')
+  avatar: string;
+
+  @Column('text')
+  bio: string;
+
+  @Column('decimal')
+  invested_amt: number;
+
+  @OneToMany(() => Comment, (comment) => comment.user)
+  comments: Comment[];
+
+  @OneToMany(() => Project, (project) => project.user)
+  projects: Project[];
+
+  @OneToMany(() => Interest, (interest) => interest.user)
+  interests: Interest[];
+
+  @OneToMany(() => Investment, (investment) => investment.user)
+  investments: Investment[];
+}
