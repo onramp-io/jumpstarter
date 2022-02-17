56d31b2151d5bfdb845825db00206ade061171e4
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-17 14:21:23 -0500

    Adding user controller methods 1. get user 2. create a user 3. update a user 4. delete a user

--- src/backend/controller/user/user.ts
+++ src/backend/controller/user/user.ts
@@ -1,11 +1,59 @@
+import {
+  get_user,
+  insert_user,
+  update_user,
+  delete_user,
+} from '@backend/services/db/user/user_db';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-const user_controller = async (
+export const get_user_controller = async (
   req: NextApiRequest,
   res: NextApiResponse,
   user
 ) => {
-  res.status(200).json({ name: user.attributes.given_name });
+  const user_bio = await get_user(req, res, user.username);
+  res.status(200).json({
+    fName: user.attributes.given_name,
+    lName: user.attributes.family_name,
+    bio: user_bio,
+  });
 };
 
-export default user_controller;
+export const post_user_controller = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  user
+) => {
+  const user_bio = await insert_user(req, res, user.username);
+  res.status(200).json({
+    fName: user.attributes.given_name,
+    lName: user.attributes.family_name,
+    bio: user_bio,
+  });
+};
+
+export const put_user_controller = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  user
+) => {
+  const user_bio = await update_user(req, res, user.username);
+  res.status(200).json({
+    fName: user.attributes.given_name,
+    lName: user.attributes.family_name,
+    bio: user_bio,
+  });
+};
+
+export const delete_user_controller = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  user
+) => {
+  const user_bio = await delete_user(req, res, user.username);
+  res.status(200).json({
+    fName: user.attributes.given_name,
+    lName: user.attributes.family_name,
+    bio: user_bio,
+  });
+};
