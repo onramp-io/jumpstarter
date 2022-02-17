55ee0655e36ad6b6608e48f4f7934d58f8fc8a10
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-17 14:16:36 -0500

    Adding firebase client/admin env And removing userpool env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   env: {
-    COGNITO_POOL_ID: process.env.COGNITO_POOL_ID,
-    COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
+    DB_URL: process.env.DB_URL,
+    DB_TYPE: process.env.DB_TYPE,
+    FB_CLIENT_API_KEY: process.env.FB_CLIENT_API_KEY,
+    FB_CLIENT_AUTH_DOMAIN: process.env.FB_CLIENT_AUTH_DOMAIN,
+    FB_CLIENT_PROJECT_ID: process.env.FB_CLIENT_PROJECT_ID,
+    FB_CLIENT_STORAGE_BUCKET: process.env.FB_CLIENT_STORAGE_BUCKET,
+    FB_CLIENT_MESSAGING_SENDER_ID: process.env.FB_CLIENT_MESSAGING_SENDER_ID,
+    FB_CLIENT_APP_ID: process.env.FB_CLIENT_APP_ID,
+    FB_ADMIN_PROJECT_ID: process.env.FB_ADMIN_PROJECT_ID,
+    FB_ADMIN_PRIVATE_KEY_ID: process.env.FB_ADMIN_PRIVATE_KEY_ID,
+    FB_ADMIN_PRIVATE_KEY: process.env.FB_ADMIN_PRIVATE_KEY,
+    FB_ADMIN_CLIENT_EMAIL: process.env.FB_ADMIN_CLIENT_EMAIL,
+    FB_ADMIN_CLIENT_ID: process.env.FB_ADMIN_CLIENT_ID,
   },
 };
