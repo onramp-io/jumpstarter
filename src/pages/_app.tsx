d926fd7c68bb7b4b5b72ddab4cae1d00329903b0
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-17 14:26:29 -0500

    Wrapping AuthProvider around app TODO: figure out how to only wrap protected routes in NextJs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '@frontend/components/layout';
-import Amplify from 'aws-amplify';
-import awsconfig from '../aws-exports';
-
-Amplify.configure({ ...awsconfig, ssr: true });
+import { PrivateRouteProvider } from '@frontend/context/AuthProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <PrivateRouteProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </PrivateRouteProvider>
   );
 }
 
