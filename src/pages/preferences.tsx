c6784272acefe9f1239db6001f18f03a00403512
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2022-02-17 09:56:45 -0800

    Create /preferences NextPage
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

---
+++ src/pages/preferences.tsx
@@ -0,0 +1,26 @@
+import { NextPageContext } from 'next';
+import axios from 'axios';
+import PreferenceCard, { ProjectCategory } from '@frontend/components/preferenceCard';
+
+interface UserPreferencesProps {
+
+}
+
+const UserPreferences = function UserPreferencesComponent<UserPreferencesProps>({}) {
+  return (
+    <>
+    <PreferenceCard imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" projectCategory={ProjectCategory.CATEGORY_1}/>
+    <PreferenceCard imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" projectCategory={ProjectCategory.CATEGORY_2}/>
+    <PreferenceCard imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" projectCategory={ProjectCategory.CATEGORY_3}/>
+    <PreferenceCard imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" projectCategory={ProjectCategory.CATEGORY_4}/>
+    <PreferenceCard imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" projectCategory={ProjectCategory.CATEGORY_5}/>
+    <PreferenceCard imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" projectCategory={ProjectCategory.CATEGORY_6}/>
+    </>
+  );
+}
+
+UserPreferences.getInitialProps = async ({ req }: NextPageContext) => {
+ return {}; 
+};
+
+export default UserPreferences;
