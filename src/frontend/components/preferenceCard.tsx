27d011fd1c15728db85cfe695f7db415e25f0968
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2022-02-17 13:54:32 -0800

    Make /preferences page responsive, fix proportions
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- src/frontend/components/preferenceCard.tsx
+++ src/frontend/components/preferenceCard.tsx
@@ -1,6 +1,6 @@
-import { NextPageContext } from 'next';
-import axios from 'axios';
-import { Box, Heading, Image } from 'grommet';
+import { NextPageContext } from "next";
+import axios from "axios";
+import { Box, Heading, Image, ResponsiveContext } from "grommet";
 
 export enum ProjectCategory {
   CATEGORY_1 = "Arts",
@@ -8,12 +8,12 @@ export enum ProjectCategory {
   CATEGORY_3 = "Film",
   CATEGORY_4 = "Food & Craft",
   CATEGORY_5 = "Games",
-  CATEGORY_6 = "Music"
+  CATEGORY_6 = "Music",
 }
 
 interface PreferenceCardProps {
-  imageUrl: string,
-  projectCategory: ProjectCategory,
+  imageUrl: string;
+  projectCategory: ProjectCategory;
 }
 
 const PreferenceCard = function preferenceCardComponent<PreferenceCardProps>({
@@ -21,39 +21,32 @@ const PreferenceCard = function preferenceCardComponent<PreferenceCardProps>({
   projectCategory,
 }) {
   return (
-      <Box
+    <Box
       elevation="medium"
-      direction="row"
+      margin={{
+        bottom: "small",
+        right: "small",
+      }}
+      direction="column"
+      height="min(350px, 100%)"
+      width="max(300px)"
       pad="medium"
-        align="center"
-        margin={{
-          top: "large",
-          left: "medium",
-          right: "medium",
-          bottom: "xsmall"
-        }}
-        width="large"
-      >
-      <Box
-        width="medium"
-        height="small"
-      >
-      {/** 
+      align="center"
+    >
+      <Box flex={false} width="small" height="small">
+        {/** 
        https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80
        * 
-       */}
+      */}
         <Image fit="cover" src={imageUrl} />
       </Box>
-        <Heading 
-        level={2}
-        margin="medium"
-        >{projectCategory}</Heading>
-      </Box>
+      <Heading className="no-text-wrap" level={2} margin="medium">
+        {projectCategory}
+      </Heading>
+    </Box>
   );
-}
-
-PreferenceCard.getInitialProps = async ({ req }: NextPageContext) => {
-
 };
+
+PreferenceCard.getInitialProps = async ({ req }: NextPageContext) => {};
 
 export default PreferenceCard;
--- src/pages/preferences.tsx
+++ src/pages/preferences.tsx
@@ -1,26 +1,77 @@
-import { NextPageContext } from 'next';
-import axios from 'axios';
-import PreferenceCard, { ProjectCategory } from '@frontend/components/preferenceCard';
+import { NextPageContext } from "next";
+import axios from "axios";
+import PreferenceCard, {
+  ProjectCategory,
+} from "@frontend/components/preferenceCard";
+import SectionHeader from "@frontend/components/sectionHeader";
+import { Box, ResponsiveContext } from "grommet";
 
-interface UserPreferencesProps {
+interface UserPreferencesProps {}
 
-}
-
-const UserPreferences = function UserPreferencesComponent<UserPreferencesProps>({}) {
+const UserPreferences = function UserPreferencesComponent<
+  UserPreferencesProps
+>({}) {
   return (
     <>
-    <PreferenceCard imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" projectCategory={ProjectCategory.CATEGORY_1}/>
-    <PreferenceCard imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" projectCategory={ProjectCategory.CATEGORY_2}/>
-    <PreferenceCard imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" projectCategory={ProjectCategory.CATEGORY_3}/>
-    <PreferenceCard imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" projectCategory={ProjectCategory.CATEGORY_4}/>
-    <PreferenceCard imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" projectCategory={ProjectCategory.CATEGORY_5}/>
-    <PreferenceCard imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" projectCategory={ProjectCategory.CATEGORY_6}/>
+      <SectionHeader
+        margin="large"
+        sectionHeader="Welcome to JumpStarter!"
+        sectionDescription="To get started, let us know what kinds of projects you're interested in seeing."
+      />
+
+      <ResponsiveContext.Consumer>
+        {(size) =>
+          size === "small" ? (
+            <Box>I am small or medium</Box>
+          ) : (
+            <Box align="center">
+              {/** 
+               edit the Box on line 31 if you need
+               to troubleshoot the cards on the
+               /preference page
+               */}
+              <Box
+                justify="center"
+                width="min(95vw, 1000px)"
+                height="min(60vw, 800px)"
+                wrap={true}
+                direction="row"
+              >
+                <PreferenceCard
+                  imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+                  projectCategory={ProjectCategory.CATEGORY_1}
+                />
+                <PreferenceCard
+                  imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+                  projectCategory={ProjectCategory.CATEGORY_2}
+                />
+                <PreferenceCard
+                  imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+                  projectCategory={ProjectCategory.CATEGORY_3}
+                />
+                <PreferenceCard
+                  imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+                  projectCategory={ProjectCategory.CATEGORY_4}
+                />
+                <PreferenceCard
+                  imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+                  projectCategory={ProjectCategory.CATEGORY_5}
+                />
+                <PreferenceCard
+                  imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+                  projectCategory={ProjectCategory.CATEGORY_6}
+                />
+              </Box>
+            </Box>
+          )
+        }
+      </ResponsiveContext.Consumer>
     </>
   );
-}
+};
 
 UserPreferences.getInitialProps = async ({ req }: NextPageContext) => {
- return {}; 
+  return {};
 };
 
 export default UserPreferences;
