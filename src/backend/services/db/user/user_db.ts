1495b45b4216ffe316fa43c9ee8b9601fdbc0f7b
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-17 22:18:52 -0500

    db query to insert/get a user

--- src/backend/services/db/user/user_db.ts
+++ src/backend/services/db/user/user_db.ts
@@ -1,17 +1,39 @@
 import connection from '@backend/config/db';
-import { User } from '@backend/model/User';
+import { User } from '@backend/entities/User';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { Request } from '@backend/middleware/verify_request';
 
-export const get_user = async (
-  req: NextApiRequest,
+export const getUser = async (
+  req: Request,
   res: NextApiResponse,
-  user
+  user: any
 ) => {
-  const bio = 'Biolakdsflasjdfl';
-  return bio;
+  const db = await connection();
+  try {
+    const email = req.user.email;
+    console.log('email', email);
+    const userData = await db
+      .createQueryBuilder()
+      .select('*')
+      .from('user', 'user')
+      .where('email = :email', { email })
+      .getRawOne();
+    console.log(userData);
+    if (userData) {
+      res.status(200).json({
+        userData,
+      });
+    } else {
+      res.status(404).json({
+        message: 'User not found',
+      });
+    }
+  } catch (error) {
+    console.log('ERROR: getUser() in user_db.ts', error);
+  }
 };
 
-export const insert_user = async (
+export const insertUser = async (
   req: NextApiRequest,
   res: NextApiResponse,
   user: any
@@ -20,34 +42,41 @@ export const insert_user = async (
   const lastName = req.body.lastName;
   const email = req.body.email;
   const uid = user.uid;
-  console.log(user);
+
   const db = await connection();
-  const newUser = await db
-    .createQueryBuilder()
-    .insert()
-    .into(User)
-    .values([
-      {
-        uid: uid,
-        first_name: firstName,
-        last_name: lastName,
-        email: email,
-        avatar: '',
-        bio: '',
-        invested_amt: 0,
-        comments: [],
-        projects: [],
-        interests: [],
-        investments: [],
-      },
-    ])
-    .execute();
-  await db.close();
-  console.log(newUser);
-  res.json(newUser);
+  try {
+    const userData = await db
+      .createQueryBuilder()
+      .insert()
+      .into(User)
+      .values([
+        {
+          first_name: firstName,
+          last_name: lastName,
+          email: email,
+          avatar: '',
+          bio: '',
+          investedAmt: 0,
+        },
+      ])
+      .execute();
+    // await db.close();
+    console.log(userData);
+    if (userData) {
+      res.status(200).json({
+        message: 'User created',
+      });
+    } else {
+      res.status(404).json({
+        message: 'User not created',
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const update_user = async (
+export const updateUser = async (
   req: NextApiRequest,
   res: NextApiResponse,
   user
@@ -56,7 +85,7 @@ export const update_user = async (
   return bio;
 };
 
-export const delete_user = async (
+export const deleteUser = async (
   req: NextApiRequest,
   res: NextApiResponse,
   user
