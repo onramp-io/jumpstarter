a17f94927f5d8e8fe767c48041f4fcb5a4515beb
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-17 21:55:28 -0500

    update db connection 1. model -> entity 2. update env vars 3. Add Like entitiy during connection 4. Clean up stale connection if any

--- src/backend/config/db.ts
+++ src/backend/config/db.ts
@@ -1,18 +1,25 @@
-import { createConnection, getConnection } from 'typeorm';
-import { User } from '../model/User';
-import { Project } from '../model/Project';
-import { Investment } from '../model/Investment';
-import { Interest } from '../model/Interest';
-import { Comment } from '../model/Comment';
+import { createConnection, getConnection, Connection } from 'typeorm';
+import { User } from '../entity/User';
+import { Project } from '../entity/Project';
+import { Investment } from '../entity/Investment';
+import { Interest } from '../entity/Interest';
+import { Comment } from '../entity/Comment';
+import { Like } from '../entity/Like';
 
 //create typeorm database connection using ormconfig.json file
 const connection = async () => {
+  try {
+    const staleConnection = getConnection();
+    await staleConnection.close();
+  } catch (error) {
+    // no stale connection to clean up
+  }
   const temp = await createConnection({
-    type: 'postgres',
-    url: process.env.DB_URL,
-    logging: true, //statements/queries console logged to terminal -> should remove when not debugging
+    type: process.env.DB_TYPE as any,
+    url: process.env.DB_CONNECTION_STRING,
+    // logging: true, //statements/queries console logged to terminal -> should remove when not debugging
     synchronize: true, //will tralslate logic to sql
-    entities: [User, Project, Investment, Interest, Comment],
+    entities: [User, Project, Investment, Comment, Like, Interest],
   });
   if (getConnection().isConnected) {
     console.log('type orm db connected');
