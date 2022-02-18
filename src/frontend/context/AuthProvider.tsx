ea84e4a19cc8e1d559b005fe35d097bb0c928737
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-17 22:14:31 -0500

    New Context method to grab user data

--- src/frontend/context/AuthProvider.tsx
+++ src/frontend/context/AuthProvider.tsx
@@ -6,17 +6,21 @@ import { onAuthStateChanged } from '@firebase/auth';
 
 import { auth } from '../../firebase/client/client';
 import { getIdToken } from 'firebase/auth';
+import axios from 'axios';
 
 export interface AuthContextType {
   access_token: string;
+  firstName: string;
 }
 
 export const AuthContext = createContext<AuthContextType>({
   access_token: '',
+  firstName: '',
 });
 
 export const PrivateRouteProvider: NextPage = ({ children }) => {
   const [access_token, setAccessToken] = useState<string>('');
+  const [firstName, setFirstName] = useState<string>('');
 
   const router = useRouter();
 
@@ -26,15 +30,26 @@ export const PrivateRouteProvider: NextPage = ({ children }) => {
         router.push('/');
       } else {
         const token = await getIdToken(user);
+        getUser(token);
         if (token) {
           setAccessToken(token);
         }
       }
     });
-  }, [router]);
+    const getUser = async (token) => {
+      const headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      };
+      const resposne = await axios.get('http://localhost:3000/api/users/user', {
+        headers,
+      });
+      setFirstName(resposne.data.userData['first_name']);
+    };
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ access_token }}>
+    <AuthContext.Provider value={{ access_token, firstName }}>
       {children}
     </AuthContext.Provider>
   );
