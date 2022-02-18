5dd19eacedbb913ec7ff041f471f03ded306df03
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-17 22:18:21 -0500

    1. removing signout from profile page 2. render user first name

--- src/pages/app/profile/index.tsx
+++ src/pages/app/profile/index.tsx
@@ -1,33 +1,19 @@
 import React from 'react';
-import { useRouter } from 'next/router';
-import { InferGetServerSidePropsType, GetServerSidePropsContext } from 'next';
 
 import { Box, Heading, Button, Text } from 'grommet';
 
 import styles from '../../../styles/Signup.module.css';
-import admin from '../../../firebase/admin/admin';
+
 import { useAuth } from '@frontend/context/AuthProvider';
 
 function MyProfile() {
-  const router = useRouter();
-  const { access_token } = useAuth();
+  const { firstName } = useAuth();
 
-  const signOut = async function () {
-    try {
-      router.push('/login');
-    } catch (error) {
-      console.log('error signing out: ', error);
-    }
-  };
   return (
     <>
       <Box className={styles.signup_wrapper}>
         <Heading>MyProfile</Heading>
-        <Heading>Hello {access_token}</Heading>
-        {access_token}
-        <Button primary onClick={signOut}>
-          Sign out
-        </Button>
+        <Heading>Hello {firstName}</Heading>
       </Box>
     </>
   );
