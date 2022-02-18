d1c099eec1f5211f0a80f5a41f5190d4d4a395ff
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-18 02:42:33 -0500

    Handler for user put request

---
+++ src/pages/api/users/update.ts
@@ -0,0 +1,26 @@
+import { verify_request } from '@backend/middleware/verify_request';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { put_user_controller } from '../../../backend/controller/user/user';
+
+interface Request extends NextApiRequest {
+  user: any;
+}
+const handler = async (req: Request, res: NextApiResponse) => {
+  try {
+    console.log(req.method);
+    switch (req.method) {
+      case 'PUT':
+        put_user_controller(req, res);
+        break;
+      default:
+        res.status(404).json({
+          message: 'Method not found',
+        });
+    }
+  } catch (error) {
+    console.log('ERROR: handler() in user.ts', error);
+    res.status(401).json({ error });
+  }
+};
+
+export default verify_request(handler);
