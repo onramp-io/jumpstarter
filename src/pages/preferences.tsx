016eeffdbfece8fa9b85a59915254b247f0d507e
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2022-02-17 16:37:52 -0800

    Refactor Preferences-Responsive Mobile breakpoints
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- src/pages/preferences.tsx
+++ src/pages/preferences.tsx
@@ -23,7 +23,54 @@ const UserPreferences = function UserPreferencesComponent<
       <ResponsiveContext.Consumer>
         {(size) =>
           size === "small" ? (
-            <Box>I am small or medium</Box>
+            <Box align="center">
+              {/** 
+               edit the Box right below this comment if you need
+               to troubleshoot the cards on the
+               /preference page
+               */}
+              <Box
+                justify="center"
+                align="center"
+                width="min(95vw, 1000px)"
+                height="min(60vw, min-content)"
+                wrap={true}
+                direction="row"
+              >
+                <PreferenceCard
+                  imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+                  projectCategory={ProjectCategory.CATEGORY_1}
+                />
+                <PreferenceCard
+                  imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+                  projectCategory={ProjectCategory.CATEGORY_2}
+                />
+                <PreferenceCard
+                  imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+                  projectCategory={ProjectCategory.CATEGORY_3}
+                />
+                <PreferenceCard
+                  imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+                  projectCategory={ProjectCategory.CATEGORY_4}
+                />
+                <PreferenceCard
+                  imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+                  projectCategory={ProjectCategory.CATEGORY_5}
+                />
+                <PreferenceCard
+                  imageUrl="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+                  projectCategory={ProjectCategory.CATEGORY_6}
+                />
+                <Box width="70%" margin="medium" gap="medium">
+                  <Button primary label="Submit my preferences" />
+                  <ReusableLink
+                    linkHref="/"
+                    linkCaption="Skip (you can set up your preferences later in your User Settings) >"
+                    className="grey-text"
+                  />
+                </Box>
+              </Box>
+            </Box>
           ) : (
             <Box align="center">
               {/** 
