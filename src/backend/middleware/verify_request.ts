c803521831e563817053dafe909bc8b712fb4596
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-17 22:12:41 -0500

    exporting custom extended Request obj

--- src/backend/middleware/verify_request.ts
+++ src/backend/middleware/verify_request.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import admin from '../../firebase/admin/admin';
 
-interface Request extends NextApiRequest {
+export interface Request extends NextApiRequest {
   user: any;
 }
 
