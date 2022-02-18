8bf2317c72500a0973bc3e0ab474e7cd908c964f
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-18 17:27:56 -0500

    Update user controller TODO: Benett suggested project structure 2. setup payout db

--- src/backend/controller/user/user.ts
+++ src/backend/controller/user/user.ts
@@ -3,12 +3,15 @@ import {
   insertUser,
   updateUser,
   deleteUser,
+  payOutUser,
 } from '@backend/services/db/user/user_db';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Request } from '@backend/middleware/verify_request';
 
 export const getUserController = async (req: Request, res: NextApiResponse) => {
+  // break down request <===========
   getUser(req, res);
+  // respond back to client with soem data <===========
 };
 
 export const postUserController = async (
@@ -27,4 +30,8 @@ export const deleteUserController = async (
   res: NextApiResponse
 ) => {
   deleteUser(req, res);
+};
+
+export const payoutController = async (req: Request, res: NextApiResponse) => {
+  payOutUser(req, res);
 };
