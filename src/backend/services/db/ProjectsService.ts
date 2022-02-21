68bad5fe3a59423abf93c9a2f1693c641f6c0f6b
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2022-02-21 13:43:07 -0800

    Add methods: ProjectService.create & .findAll
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- src/backend/services/db/ProjectsService.ts
+++ src/backend/services/db/ProjectsService.ts
@@ -2,37 +2,77 @@ import connection from "@backend/config/db";
 import { Project } from "@backend/entities/Project";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Request } from "@backend/middleware/verify_request";
+import prepareDbConnection from "@backend/lib/prepareDbConnection";
+import { Db, getConnection } from "typeorm";
+import ProjectsPayload from "@backend/common/ProjectsPayload";
 
 const ProjectService = {
   /**
    * CREATE: 'POST' request to **Insert ONE Record**
    */
-  create: () => {},
+  create: async ({
+    pictures,
+    title,
+    category,
+    description,
+    fundTiers,
+    currFundGoal,
+    launchDate,
+    user,
+  }: ProjectsPayload["create"]) => {
+    await prepareDbConnection();
+
+    const projectInsertResult = await getConnection()
+      .createQueryBuilder()
+      .insert()
+      .into(Project)
+      .values({
+        pictures,
+        title,
+        category,
+        description,
+        fundTiers,
+        currFundGoal,
+        launchDate,
+        user,
+      })
+      .execute();
+
+    return projectInsertResult;
+  },
 
   /**
    * READ: 'GET' request for **ALL** Records
    */
-  findAll: () => {},
+  findAll: async () => {
+    await prepareDbConnection();
+
+    const allProjectRows: Project[] = await getConnection()
+      .createQueryBuilder()
+      .getMany();
+
+    return allProjectRows;
+  },
 
   /**
    * READ: 'GET' request for **ONE Record by ID**
    */
-  findById: () => {},
+  findById: async () => {},
 
   /**
    * UPDATE: 'PUT' request for ONE Record by ID
    */
-  updateById: () => {},
+  updateById: async () => {},
 
   /**
    * DESTROY: 'DELETE' request for ONE Record by ID
    */
-  deleteById: () => {},
+  deleteById: async () => {},
 
   /**
    * READ: 'GET' request for **ALL** Records of a Particular Category
    */
-  filterByCategory: (category) => {},
+  filterByCategory: async (category) => {},
 };
 
 export default ProjectService;
