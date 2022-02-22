81cfa535126b92c7396ab4845ef31264a5a4148e
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-22 11:59:22 -0500

    Controller to get a user's investments

--- src/backend/controller/investment/investment.ts
+++ src/backend/controller/investment/investment.ts
@@ -1,10 +1,38 @@
 import {
-    addNewInvestment
-  } from '@backend/services/db/investment/investment_db';
-  import type { NextApiRequest, NextApiResponse } from 'next';
-  import { Request } from '@backend/middleware/verify_request';
-  
-  export const addNewInvestmentController = async (req: Request, res: NextApiResponse) => {
-    addNewInvestment(req, res);
-  };
-  
+  addNewInvestment,
+  getUserInvestments,
+} from '@backend/services/db/investment/investment_db';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Request } from '@backend/middleware/verify_request';
+
+export const addNewInvestmentController = async (
+  req: Request,
+  res: NextApiResponse
+) => {
+  addNewInvestment(req, res);
+};
+
+export const getUserInvestmentController = async (
+  req: Request,
+  res: NextApiResponse
+) => {
+  try {
+    const { email } = req.user;
+    const userInvestments = await getUserInvestments(email);
+    if (userInvestments) {
+      res.status(200).json({
+        userInvestments,
+      });
+    } else {
+      res.status(404).json({
+        message: 'No investments found',
+      });
+    }
+  } catch (error) {
+    console.log(
+      'ERROR: @getUserInvestmentController api/investment/investments.ts',
+      error
+    );
+    res.status(401).json({ error });
+  }
+};
