0e2919f62e22cc2d5f75ab7117ec75f75cd29eb6
Marri Kang <mkang@bl-mkang-w3704q2tjn.lan>
2022-02-22 16:08:30 -0500

    Pass initial state in from page, add validations

--- src/frontend/components/projectForm.tsx
+++ src/frontend/components/projectForm.tsx
@@ -2,56 +2,107 @@ import { Box, Button, DateInput, FileInput, Form, FormField, Heading, Select, Te
 import type { NextPage } from 'next';
 import { useState } from 'react';
 
-const ProjectForm: NextPage = () => {
-  const [projectInfo, setProjectInfo] = useState();
-  const [category, setCategory] = useState('Film');
+type projectFormType = {
+  title: string,
+  category: string,
+  description: string,
+  end_date: string,
+  fund_goal: number,
+  fund_tiers: number[],
+  pictures: string[],
+}
+
+interface ProjectFormProps {
+    projectFormState: projectFormType
+}
+
+const ProjectForm: NextPage<ProjectFormProps> = ({ projectFormState }): JSX.Element => {
+
+  const [projectState, setProjectState] = useState(projectFormState);
+  const [imageFile, setImageFile] = useState<File>();
+  const [valid, setValid] = useState('false');
+
+  const submitProject = () => {
+
+  }
+
+  const validateForm = () => {
+    const tier1 = projectState.fund_tiers[1];
+    const tier2 = projectState.fund_tiers[2];
+    const tier3 = projectState.fund_tiers[3];
+    const goal = projectState.fund_goal;
+    let errors = [];
+
+    if (tier1 >= tier2) {
+      errors.push("Tier 1 must be less than Tier 2.")
+    }
+
+    if (tier1 >= tier3) {
+      errors.push("Tier 1 must be less than Tier 3.")
+    }
+
+    if (tier2 >= tier3) {
+      errors.push("Tier 2 must be less than Tier 3.")
+    }
+
+    if (tier1 >= goal || tier2 >= goal || tier3 >= goal) {
+      errors.push("Fund tier amounts must not exceed fund goal amount.")
+    }
+
+    if (tier1 <= 0 || tier2 <= 0 || tier3 <= 0) {
+      errors.push("Fund tiers must be greater than 0.")
+    }
+  }
 
   return (
     <Box margin={{ horizontal: "15rem", vertical: "3rem" }}>
       <Form
-        value={projectInfo}
-        onChange={info => setProjectInfo(info)}
+        value={projectState}
         onSubmit={({ value }) => { }}
+        validate="blur"
+        onValidate={() => { }}
       >
         <Box direction="column" gap="xlarge">
-          <FormField label="Upload cover image">
+          <FormField label="Upload cover image" required>
             <Box margin={{horizontal: "10rem"}}>
-              <FileInput />
+              <FileInput onChange={(event) => { setImageFile(event.target.files[0])}}/>
             </Box>
           </FormField>
 
-          <FormField label="Project Title">
-            <TextInput />
+          <FormField label="Project Title" required>
+            <TextInput onChange={(event) => { setProjectState({...projectState, title: event.target.value}) }}/>
           </FormField>
 
-          <FormField label="Project Category">
+          <FormField label="Project Category" required>
             <Select
               options={['Film', 'Tech', 'Games', 'Music', 'Literature', 'Food']}
-              value={category}
-              onChange={({ option }) => setCategory(option)}
+              onChange={({ option }) => setProjectState({...projectState, category: option})}
             />
           </FormField>
 
-          <FormField label="Project Description">
-            <TextArea />
+          <FormField label="Project Description" required>
+            <TextArea onChange={(event) => { setProjectState({...projectState, description: event.target.value}) }}/>
           </FormField>
 
-          <FormField label="Target Date">
-            <DateInput format="mm/dd/yyyy"/>
+          <FormField label="Target Date" required>
+            <DateInput format="mm/dd/yyyy" onChange={(event) => setProjectState({...projectState, end_date: event.value.toString()})} />
           </FormField>
 
-          <FormField label="Funding Goal">
-            <TextInput />
+          <FormField label="Funding Goal"required>
+            <TextInput type="number" onChange={(event) => { setProjectState({...projectState, fund_goal: Number(event.target.value)}) }}/>
           </FormField>
 
-          <FormField label="Funding Tiers">
+          <FormField
+            label="Funding Tiers"
+            required
+            validate={[]}>
             <Box margin={{left: "small"}}>
               <Text margin={{vertical: "small"}}>Tier 1</Text>
-              <TextInput placeholder="$0"/>
+              <TextInput type="number" placeholder="$0" value={projectState.fund_tiers[1]}/>
               <Text margin={{ vertical: "small" }}>Tier 2</Text>
-              <TextInput placeholder="$0"/>
+              <TextInput type="number" placeholder="$0" value={projectState.fund_tiers[2]}/>
             <Text margin={{ vertical: "small" }}>Tier 3</Text>
-              <TextInput placeholder="$0"/>
+              <TextInput type="number" placeholder="$0" value={projectState.fund_tiers[3]}/>
             </Box>
           </FormField>
 
--- src/pages/app/edit.tsx
+++ src/pages/app/edit.tsx
@@ -2,15 +2,15 @@ import { Box, Heading } from 'grommet';
 import type { NextPage } from 'next';
 import ProjectForm from '@frontend/components/projectForm';
 
-const CreateProject: NextPage = () => {
+const EditProject: NextPage = () => {
   const projectFormDetails = {
-    title: "",
-    category: "",
-    description: "",
-    end_date: "",
-    fund_goal: 0,
-    fund_tiers: [0],
-    pictures: []
+    title: "Current Title",
+    category: "Film",
+    description: "Current description.",
+    end_date: "01/01/2030",
+    fund_goal: 10000,
+    fund_tiers: [0, 100, 1000, 2000],
+    pictures: ["weh"]
   }
 
   return (
@@ -23,4 +23,4 @@ const CreateProject: NextPage = () => {
   );
 };
 
-export default CreateProject;
+export default EditProject;
