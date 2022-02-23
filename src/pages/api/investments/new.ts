0e5814fa205135471cbb5484272254849e6e4a73
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-22 19:24:14 -0500

    updating code to @pmaganty

--- src/pages/api/investments/new.ts
+++ src/pages/api/investments/new.ts
@@ -1,21 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { addNewInvestmentController } from '../../../backend/controller/investment/investment';
+import { InvestmentController } from '../../../backend/controller/investment/investment';
 
 type Data = {
-    name: string;
+  name: string;
 };
 
 interface Request extends NextApiRequest {
-    user: any;
+  user: any;
 }
 
-export default function handler(
-    req: Request,
-    res: NextApiResponse<Data>
-) {
-    //Add new user to database if user does not already exist
-    switch(req.method) {
-        case 'POST': addNewInvestmentController(req, res);
-        default: console.log(req.body);
+export default function handler(req: Request, res: NextApiResponse<Data>) {
+  //Add new user to database if user does not already exist
+  try {
+    switch (req.method) {
+      case 'POST':
+        InvestmentController.create(req)
+          .then((response) => {
+            res.status(200).json(response.data);
+          })
+          .catch((err) => {
+            res.status(500).json(response.err);
+          });
+        break;
+      default:
+        console.log(req.body);
     }
+  } catch (error) {}
 }
