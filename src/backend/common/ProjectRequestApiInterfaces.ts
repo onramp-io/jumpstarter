efdf012836e4d133ad7242225adcaf55ef7e16f0
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2022-02-22 22:20:05 -0800

    Refactor RequestApiInterfaces for Reuse/Modularity
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- src/backend/common/ProjectRequestApiInterfaces.ts
+++ src/backend/common/ProjectRequestApiInterfaces.ts
@@ -6,120 +6,149 @@ import { NextApiRequest } from "next";
 
 export type sortByStringType = "createdDate" | "likesAmt" | "userId";
 
+export interface CreateParamsInterface {
+  title: string;
+  category: string;
+  description: string;
+  fundTiers: number[];
+  currFundGoal: number;
+  user: User;
+  // Is launchdate required soon as proj is being created?
+  // if not, pls remove the next line.
+  launchDate?: string;
+}
+
 export interface ProjectCreateApiRequest extends NextApiRequest {
   body: {
-    createParams: {
-      title: string;
-      category: string;
-      description: string;
-      fundTiers: number[];
-      currFundGoal: number;
-      user: User;
-      // Is launchdate required soon as proj is being created?
-      // if not, pls remove the next line.
-      launchDate?: string;
-    };
+    createParams: CreateParamsInterface;
   };
+}
+
+export interface FindAllParamsInterface {
+  undefined?: undefined | null;
 }
 
 export interface ProjectFindAllApiRequest extends NextApiRequest {
   body: {
-    findAllParams: {
-      undefined?: undefined | null;
-    };
+    findAllParams: FindAllParamsInterface;
   };
+}
+
+export interface FindByIdParamsInterface {
+  id: string;
 }
 
 export interface ProjectFindByIdApiRequest extends NextApiRequest {
   body: {
-    findByIdParams: {
-      id: string;
-    };
+    findByIdParams: FindAllParamsInterface;
   };
+}
+
+export interface UpdateByIdParamsInterface {
+  id: string;
+  pictures?: string[];
+  title?: string;
+  category?: string;
+  description?: string;
+  fundTiers?: number[];
+  currFundGoal?: number;
+  comments?: Comment[];
+  investments?: Investment[];
+  likes?: Like[];
+  // Is launchdate required soon as proj is being created?
+  // if not, pls remove the next line.
+  launchDate?: string;
 }
 
 export interface ProjectUpdateByIdApiRequest extends NextApiRequest {
   body: {
-    updateByIdParams: {
-      id: string;
-      pictures?: string[];
-      title?: string;
-      category?: string;
-      description?: string;
-      fundTiers?: number[];
-      currFundGoal?: number;
-      comments?: Comment[];
-      investments?: Investment[];
-      likes?: Like[];
-      // Is launchdate required soon as proj is being created?
-      // if not, pls remove the next line.
-      launchDate?: string;
-    };
+    updateByIdParams: UpdateByIdParamsInterface;
   };
+}
+
+export interface DeleteByIdParamsInterface {
+  id: string;
 }
 
 export interface ProjectDeleteByIdApiRequest extends NextApiRequest {
   body: {
-    deleteByIdParams: {
-      id: string;
-    };
+    deleteByIdParams: DeleteByIdParamsInterface;
   };
 }
 
-export interface ProjectSortByApiRequest extends NextApiRequest {
-  body: {
-    sortByString: sortByStringType;
-
-    sortByParams: {
-      column: sortByStringType;
-    };
-  };
-}
-
-const ProjectPayload = {
-  create: {
-    pictures,
-    title,
-    category,
-    description,
-    fundTiers,
-    currFundGoal,
-    user,
-    // Is launchdate required soon as proj is being created?
-    // if not, pls remove the next line.
-    launchDate,
-  },
-
-  findAll: undefined,
-
-  findById: {
-    id,
-  },
-
-  updateById: {
-    id,
-    pictures,
-    title,
-    category,
-    description,
-    fundTiers,
-    currFundGoal,
-    comments,
-    investments,
-    likes,
-    // Is launchdate required soon as proj is being created?
-    // if not, pls remove the next line.
-    launchDate,
-  },
-
-  deleteById: {
-    id,
-  },
-
-  sortBy: {
-    // TODO: (Tapa) Add fields as needed for recommendation algorithm
-    column,
-  },
+export const SortByString = {
+  NEWEST: "createdDate",
+  TRENDING: "likesAmt", // TODO: Add column you want to sort by (instead of "likesAmt") @Pran
+  RECOMMENDED: "user.id", // TODO: Add column you want to sort by (instead of "user.id") @Tapa
 };
 
-export default ProjectPayload;
+export enum SortByStringEnum {
+  NEWEST = "createdDate",
+  TRENDING = "likesAmt", // TODO: Add column you want to sort by (instead of "likesAmt") @Pran
+  RECOMMENDED = "user.id", // TODO: Add column you want to sort by (instead of "user.id") @Tapa
+}
+// SortByString.NEWEST
+// SortByString.TRENDING
+// SortByString.RECOMMENDED
+
+export type SortByParamsType = {
+  SortByString:
+    | SortByStringEnum.NEWEST
+    | SortByStringEnum.TRENDING
+    | SortByStringEnum.RECOMMENDED;
+};
+
+export interface ProjectSortByApiRequest extends NextApiRequest {
+  body: SortByParamsInterface;
+}
+
+/** 
+ * 
+ const ProjectPayload = {
+   create: {
+     pictures,
+     title,
+     category,
+     description,
+     fundTiers,
+     currFundGoal,
+     user,
+     // Is launchdate required soon as proj is being created?
+     // if not, pls remove the next line.
+     launchDate,
+   },
+ 
+   findAll: undefined,
+ 
+   findById: {
+     id,
+   },
+ 
+   updateById: {
+     id,
+     pictures,
+     title,
+     category,
+     description,
+     fundTiers,
+     currFundGoal,
+     comments,
+     investments,
+     likes,
+     // Is launchdate required soon as proj is being created?
+     // if not, pls remove the next line.
+     launchDate,
+   },
+ 
+   deleteById: {
+     id,
+   },
+ 
+   sortBy: {
+     // TODO: (Tapa) Add fields as needed for recommendation algorithm
+     column,
+   },
+ };
+ 
+ export default ProjectPayload;
+ */
