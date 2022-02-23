5c39bc0b7f893dbedb5bfb78d0cc26726825a0ac
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-22 19:23:52 -0500

    refactoring investments/get handler

--- src/pages/api/investments/get.ts
+++ src/pages/api/investments/get.ts
@@ -1,6 +1,13 @@
-import { getUserInvestmentController } from '@backend/controller/investment/investment';
 import { verifyRequest } from '@backend/middleware/verify_request';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { InvestmentController } from '@backend/controller/investment/investment';
+
+import {
+  ReasonPhrases,
+  StatusCodes,
+  getReasonPhrase,
+  getStatusCode,
+} from 'http-status-codes';
 
 interface Request extends NextApiRequest {
   user: any;
@@ -9,16 +16,26 @@ const handler = async (req: Request, res: NextApiResponse) => {
   try {
     switch (req.method) {
       case 'GET':
-        getUserInvestmentController(req, res);
+        const investments = await InvestmentController.get(req);
+        if (investments) {
+          res.status(StatusCodes.OK).json({
+            message: 'Investments found',
+            investments,
+          });
+        } else {
+          res.status(StatusCodes.NOT_FOUND).json({
+            message: 'Investments not found',
+          });
+        }
         break;
       default:
-        res.status(404).json({
+        res.status(StatusCodes.METHOD_NOT_ALLOWED).json({
           message: 'Method not found',
         });
     }
   } catch (error) {
     console.log('ERROR: handler() in api/investments/get.ts', error);
-    res.status(401).json({ error });
+    res.status(StatusCodes.UNAUTHORIZED).json({ error });
   }
 };
 
