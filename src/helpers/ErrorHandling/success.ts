b138ea9dc8d60692028de1eddeeb4d71e80d7ad8
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-24 06:26:01 -0500

    Removing success class, making it a simple object

--- src/helpers/ErrorHandling/success.ts
+++ src/helpers/ErrorHandling/success.ts
@@ -1,19 +1,5 @@
-export class Success {
-  code: number;
-  status: string;
-  message: string;
-
-  constructor(message: string) {
-    this.code = 200;
-    this.status = 'OK';
-    this.message = message;
-  }
-
-  toJSON() {
-    return {
-      code: this.code,
-      status: this.status,
-      message: this.message,
-    };
-  }
-}
+export const Success = {
+  code: 200,
+  status: 'OK',
+  message: 'Success',
+};
