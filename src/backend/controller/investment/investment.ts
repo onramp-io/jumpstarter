1bb766fbecae3720e1f177ba29053b0aeafb6999
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-24 07:49:47 -0500

    Updating InvestmentController 1. Removing create() because @pmaganty handling it 2. Destructured uid from req.user

--- src/backend/controller/investment/investment.ts
+++ src/backend/controller/investment/investment.ts
@@ -1,16 +1,12 @@
-import { InvestmentsDbService } from '@backend/services/db/investment/investment_db';
+import { InvestmentsDbService } from '@backend/services/db/investment/investmentService';
 import { Request } from '@backend/middleware/verify_request';
 
 export const InvestmentController = {
-  create: async (req: Request) => {
-    const { userId, projectId, fundAmt } = req.body;
-    const data = await InvestmentsDbService.create(userId, projectId, fundAmt);
-    return data;
-  },
-
-  get: async (req: Request) => {
-    const { email } = req.user;
-    const userInvestments = await InvestmentsDbService.get(email);
+  getAll: async (req: Request) => {
+    const {
+      user: { uid },
+    } = req;
+    const userInvestments = InvestmentsDbService.getAll(uid);
     return userInvestments;
   },
 };
