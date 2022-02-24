16975e2c0cf008a5c5e721fbcd8f60aea8720098
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-24 06:28:22 -0500

    Updating users/update.ts to error classes

--- src/pages/api/users/update.ts
+++ src/pages/api/users/update.ts
@@ -2,14 +2,12 @@ import { verifyRequest } from '@backend/middleware/verify_request';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { UserController } from '../../../backend/controller/user/user';
 
-import {
-  ReasonPhrases,
-  StatusCodes,
-  getReasonPhrase,
-  getStatusCode,
-} from 'http-status-codes';
-
 import chalk from 'chalk';
+import {
+  MethodNotAllowedError,
+  NotFoundError,
+} from 'helpers/ErrorHandling/errors';
+import { Success } from 'helpers/ErrorHandling/success';
 
 interface Request extends NextApiRequest {
   user: any;
@@ -18,20 +16,32 @@ const handler = async (req: Request, res: NextApiResponse) => {
   try {
     switch (req.method) {
       case 'PUT':
-        const userData = await UserController.put(req);
-        res.status(StatusCodes.OK).json({
-          message: 'User updated',
-          userData,
+        await UserController.put(req);
+        res.status(Success.code).json({
+          status: Success.status,
+          message: Success.message,
+          data: {},
         });
         break;
       default:
-        res.status(StatusCodes.METHOD_NOT_ALLOWED).json({
-          message: 'Method not found',
-        });
+        throw new MethodNotAllowedError('Method not found');
     }
   } catch (error) {
-    console.log(chalk.red.bold('ERROR: handler() in user/update.ts'), error);
-    res.status(StatusCodes.UNAUTHORIZED).json({ error });
+    if (error instanceof NotFoundError) {
+      console.log(
+        chalk.red.bold(error.name + '@user/update.ts on Line 32'),
+        error.message
+      );
+    } else if (error instanceof MethodNotAllowedError) {
+      console.log(
+        chalk.red.bold(error.name + '@user/update.ts on Line 37'),
+        error.message
+      );
+      res.status(error.code).json({
+        status: error.status,
+        message: error.message,
+      });
+    }
   }
 };
 
