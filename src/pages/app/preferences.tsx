2b8b6b199f5c8ee82cfb5e508ab88fee407bb213
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-25 12:10:18 -0500

    Preferences page endering preference component

--- src/pages/app/preferences.tsx
+++ src/pages/app/preferences.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next';
 import { Box, Heading } from 'grommet';
 import styles from '../../styles/Signup.module.css';
+import UserPreferences from '@frontend/components/preferencesComponent';
 
 const Preferences: NextPage = () => {
   return (
     <>
       <Box className={styles.signup_wrapper}>
         <Heading>Preferences</Heading>
+        <UserPreferences />
       </Box>
     </>
   );
