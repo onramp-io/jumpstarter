d31724f65621c8c9af688bdeac1509c8032b9284
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-25 12:08:48 -0500

    adding two more error classes

--- src/helpers/ErrorHandling/errors.ts
+++ src/helpers/ErrorHandling/errors.ts
@@ -19,11 +19,54 @@ export class DatabaseError extends Error {
     };
   }
 }
+export class QueryError extends Error {
+  code: number;
+  status: string;
+
+  constructor(message: string) {
+    super(message);
+    this.name = 'QueryError';
+    this.message = message;
+    this.code = 500;
+    this.status = 'Internal Server Error';
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      code: this.code,
+      status: this.status,
+      message: this.message,
+    };
+  }
+}
 
 export class UserFacingError extends Error {
   constructor(message: string) {
     super(message);
     this.name = 'UserFacingError';
+  }
+}
+
+export class BadRequestError extends UserFacingError {
+  code: number;
+  status: string;
+
+  constructor(message: string) {
+    super(message);
+    this.name = 'BadRequestError';
+    this.message = message;
+    this.code = 400;
+    this.status = 'Bad Request';
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      code: this.code,
+      status: this.status,
+      message: this.message,
+    };
   }
 }
 
