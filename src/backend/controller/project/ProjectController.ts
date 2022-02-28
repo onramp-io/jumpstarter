20ff330ad56abbee0cf0334f31e4eb1ceaaa7d81
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2022-02-28 13:12:08 -0800

    Update ProjectController to require uid (auth) calling Protected Service
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- src/backend/controller/project/ProjectController.ts
+++ src/backend/controller/project/ProjectController.ts
@@ -40,6 +40,12 @@ const ProjectController = {
  
        * 
        */
+      if (req.user.uid === undefined || req.user.uid === null) {
+        throw new AuthorizationError(
+          "Auth token does not exist or has expired"
+        );
+      }
+
       const createParamsWithUid = {
         ...req.body,
         uid: req.user.uid,
@@ -55,7 +61,7 @@ const ProjectController = {
        */
       if (
         isAllTruthy(createParamsWithUid) &&
-        Object.keys(createParamsWithUid).length === 7 // <--- used to be 7 with id.
+        Object.keys(createParamsWithUid).length >= 7 // <--- used to be 7 with id.
       ) {
         const createdProj = await ProjectService.create(createParamsWithUid);
         return [createdProj, StatusCodes.CREATED];
@@ -69,10 +75,6 @@ const ProjectController = {
          * 
          */
       } else {
-        if (createParamsWithUid.uid === undefined) {
-          throw new AuthorizationError("Auth token has expired");
-        }
-
         throw new BadRequestError("Missing params on req.body");
         // return [null, StatusCodes.BAD_REQUEST]; // 400 (client error, since they forgot to pass in the params needed)
       }
@@ -91,98 +93,186 @@ const ProjectController = {
   },
 
   // READ - all
-  findAll: async (req: ProjectFindAllApiRequest) => {
+  findAll: async (req) => {
     try {
-      return await ProjectService.findAll(undefined);
+      return await ProjectService.findAll();
     } catch (err) {
-      console.warn(err.message);
-      return [null, StatusCodes.BAD_REQUEST]; // 400 (client error)
+      console.warn(
+        chalk.bgRed(`Error caught at ProjectController - ${err.message}`)
+      );
+      throw err;
+      // return [null, StatusCodes.BAD_REQUEST]; // 400 (client error)
     }
   },
 
   // READ - 1
   findById: async (req) => {
-    console.log(req.query);
-    console.log(isAllTruthy(req.query));
-    console.log(Object.keys(req.query).length === 1);
-    if (isAllTruthy(req.query) && Object.keys(req.query).length === 1) {
-      return await ProjectService.findById(req.query);
-    } else {
-      console.warn("req.query is missing id (project id)");
-      throw new Error("req.query is missing id (project id). BAD REQUEST.");
-      return [null, StatusCodes.BAD_REQUEST];
+    try {
+      /**
+       console.log(req.query);
+       console.log(isAllTruthy(req.query));
+       console.log(Object.keys(req.query).length === 1);
+       *
+       */
+      if (Object.keys(req.query).length === 1) {
+        return await ProjectService.findById(req.query);
+      } else {
+        throw new BadRequestError(
+          "req.query is missing id (project id) - Did you forget to add the id on the endpoint? e.g. /api/project/10"
+        );
+        // return [null, StatusCodes.BAD_REQUEST];
+      }
+    } catch (err) {
+      console.warn(
+        chalk.bgRed(`Error caught at ProjectController - ${err.message}`)
+      );
+      throw err;
     }
   },
 
   // UPDATE - 1
   updateById: async (req) => {
-    console.log(`you're at ProjectController.updateById( )!`);
-    console.log(
-      `req.query is composed of ${JSON.stringify(Object.keys(req.query))}`
-    );
-    console.log(
-      `req.body is composed of ${JSON.stringify(Object.keys(req.body))} `
-    );
+    try {
+      /** 
+       console.log(`you're at ProjectController.updateById( )!`);
+       console.log(
+         `req.query is composed of ${JSON.stringify(Object.keys(req.query))}`
+       );
+       console.log(
+         `req.body is composed of ${JSON.stringify(Object.keys(req.body))} `
+       );
+       * 
+       */
 
-    /** 
-     console.log(isAllTruthy(req.body));
-     console.log(isAllTruthy(req.query));
-     console.log(Object.keys(req.body).length === 8);
-     console.log(Object.keys(req.query).length === 1);
-     console.log(Object.keys(req.query.id).length);
-     * 
-     */
-    if (
-      isAllTruthy(req.body) &&
-      isAllTruthy(req.query) &&
-      Object.keys(req.body).length === 8 &&
-      Object.keys(req.query).length === 1
-    ) {
-      console.log(
-        `In ProjectService.updateById(), req.body, req.query,id is indeed all truthy and complete (req.body has complete params!)`
+      /** 
+       console.log(isAllTruthy(req.body));
+       console.log(isAllTruthy(req.query));
+       console.log(Object.keys(req.body).length === 8);
+       console.log(Object.keys(req.query).length === 1);
+       console.log(Object.keys(req.query.id).length);
+       * 
+       */
+      if (req.user.uid === undefined || req.user.uid === null) {
+        throw new AuthorizationError(
+          "Auth token does not exist or has expired"
+        );
+      }
+
+      const updateParamsWithUid = {
+        ...req.body,
+        uid: req.user.uid,
+      };
+
+      if (
+        isAllTruthy(req.body) &&
+        Object.keys(req.body).length >= 8 &&
+        Object.keys(req.query).length === 1
+      ) {
+        /** 
+         console.log(
+           `In ProjectService.updateById(), req.body, req.query,id is indeed all truthy and complete (req.body has complete params!)`
+         );
+         * 
+         */
+        return ProjectService.updateById(req.body, req.query.id);
+      } else {
+        if (Object.keys(req.query).length < 1) {
+          throw new BadRequestError(
+            "req.query is missing id (project id) - Did you forget to add the id on the endpoint? e.g. /api/project/10"
+          );
+        }
+
+        if (Object.keys(req.body).length < 8) {
+          throw new BadRequestError("req.body has missing parameters.");
+        }
+      }
+    } catch (err) {
+      console.warn(
+        chalk.bgRed(`Error caught at ProjectController - ${err.message}`)
       );
-      return ProjectService.updateById(req.body, req.query.id);
-    } else {
-      throw new Error(
-        "Error: req.body, req.query,id is missing some params. BAD REQUEST."
-      );
+      throw err;
     }
   },
 
-  //
-
   // DESTROY - 1
   deleteById: async (req) => {
-    console.log(`you're at ProjectController.deleteById( )!`);
-    console.log(`req.query looks like ==> ${JSON.stringify(req.query)}`);
-    console.log(
-      `req.query.id is composed of ${JSON.stringify(
-        Object.keys(req.query.id)
-      )} and its length is ${req.query.id.length}`
-    );
+    /** 
+     console.log(`you're at ProjectController.deleteById( )!`);
+     console.log(`req.query looks like ==> ${JSON.stringify(req.query)}`);
+     console.log(
+       `req.query.id is composed of ${JSON.stringify(
+         Object.keys(req.query.id)
+       )} and its length is ${req.query.id.length}`
+     );
+     * 
+     */
+    try {
+      if (req.user.uid === undefined || req.user.uid === null) {
+        throw new AuthorizationError(
+          "Auth token does not exist or has expired"
+        );
+      }
+      const deleteParamsWithUid = {
+        ...req.body,
+        uid: req.user.uid,
+      };
+      /** 
+       console.log(chalk.bgBlueBright(`req.user.uid is.. ${req.user.uid}`));
+       console.log(
+         chalk.bgBlueBright(
+           `deleteParamsWithUid.uid is.. ${deleteParamsWithUid.uid}`
+         )
+       );
+       * 
+       */
+      // console.log(deleteParamsWithUid.uid, "IS THE UID");
 
-    if (isAllTruthy(req.query.id) && Object.keys(req.query).length === 1) {
-      console.log(
-        `In ProjectService.deleteById(), req.body is indeed all truthy and complete (req.query.id has complete params!)`
+      if (Object.keys(req.query).length === 1) {
+        return await ProjectService.deleteById(req.query);
+      } else {
+        throw new BadRequestError(
+          "req.query is missing id (project id) - Did you forget to add the id on the endpoint? e.g. /api/project/10"
+        );
+        // return [null, StatusCodes.BAD_REQUEST];
+      }
+    } catch (err) {
+      console.warn(
+        chalk.bgRed(`Error caught at ProjectController - ${err.message}`)
       );
-      return ProjectService.deleteById(req.query.id);
-    } else {
-      throw new Error(
-        "Error: req.query.id is missing some params (id)! BAD REQUEST."
-      );
-      return [null, StatusCodes.BAD_REQUEST];
+      throw err;
     }
+    /** 
+       if (isAllTruthy(req.query.id) && Object.keys(req.query).length === 1) {
+         console.log(
+           `In ProjectService.deleteById(), req.body is indeed all truthy and complete (req.query.id has complete params!)`
+         );
+         return ProjectService.deleteById(req.query.id);
+       } else {
+         throw new Error(
+           "Error: req.query.id is missing some params (id)! BAD REQUEST."
+         );
+         return [null, StatusCodes.BAD_REQUEST];
+       }
+       * 
+       */
   },
 
   // SORT - all (TODO: add logic to ProjectService.sortBy -- Tapa & Pran)
   sortBy: async (req) => {
-    if (isAllTruthy(req.body) && Object.keys(req.body).length === 1) {
-      return ProjectService.sortBy(req);
-    } else {
-      throw new Error(
-        "req.body is missing sortBy params! (i.e. SortByString.NEWEST, or SortByString.RECOMMENDED, or SortByString.TRENDING"
+    try {
+      if (isAllTruthy(req.body) && Object.keys(req.body).length === 1) {
+        return ProjectService.sortBy(req);
+      } else {
+        throw new Error(
+          "req.body is missing sortBy params! (i.e. SortByString.NEWEST, or SortByString.RECOMMENDED, or SortByString.TRENDING"
+        );
+        return [null, StatusCodes.BAD_REQUEST];
+      }
+    } catch (err) {
+      console.warn(
+        chalk.bgRed(`Error caught at ProjectController - ${err.message}`)
       );
-      return [null, StatusCodes.BAD_REQUEST];
+      throw err;
     }
   },
 };
