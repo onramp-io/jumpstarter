8a45f78cd2d2a099b047122c5c2dbdfba012bca4
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-28 12:51:46 -0500

    Refactoring Preference component 1. Fetching categories list using useEffect

--- src/pages/app/preferences.tsx
+++ src/pages/app/preferences.tsx
@@ -2,14 +2,27 @@ import type { NextPage } from 'next';
 import { Box, Heading } from 'grommet';
 import styles from '../../styles/Signup.module.css';
 import UserPreferences from '@frontend/components/preferencesComponent';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Preferences: NextPage = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const res = await axios.get('/api/categories');
+        setCategories(res.data['categoriesList']);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCategories();
+  }, []);
+
   return (
     <>
-      <Box className={styles.signup_wrapper}>
-        <Heading>Preferences</Heading>
-        <UserPreferences />
-      </Box>
+      <UserPreferences categories={categories} />
     </>
   );
 };
