1fe733fd6632df47747b81a4c9a2a8e5ceb8412f
Tapabrata Dey <tapabratadey02@gmail.com>
2022-02-28 08:37:18 -0500

    Adding a GET route handler for categories route

--- src/pages/api/categories/index.ts
+++ src/pages/api/categories/index.ts
@@ -1,9 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import categoryController from '@backend/controller/category/categoryController';
 import { Rss } from 'grommet-icons';
-import {
-  MethodNotAllowedError,
-} from 'helpers/ErrorHandling/errors';
+import { MethodNotAllowedError } from 'helpers/ErrorHandling/errors';
 import { Success } from 'helpers/ErrorHandling/success';
 
 interface Request extends NextApiRequest {
@@ -11,34 +9,33 @@ interface Request extends NextApiRequest {
 }
 
 const handler = async (req: Request, res: NextApiResponse) => {
-  let response;
   try {
-    switch(req.method) {
-      // go to the get routes file import it from routes/get.ts
-      //Basic ping/pong endpoint to test server functionality
+    switch (req.method) {
       case 'GET':
-        response = await categoryController.getAll(req)
+        const categoriesList = await categoryController.getAll(req);
+        console.log(categoriesList);
         res.status(Success.code).json({
           status: Success.status,
           message: Success.message,
+          categoriesList,
         });
         break;
       case 'POST':
-        response = await categoryController.create(req)
+        const response = await categoryController.create(req);
         res.status(Success.code).json({
           status: Success.status,
           message: Success.message,
         });
         break;
-      default:throw new MethodNotAllowedError('Method not found');
+      default:
+        throw new MethodNotAllowedError('Method not found');
     }
-  }
-  catch (error) {
+  } catch (error) {
     res.status(error.code).json({
       status: error.status,
       message: error.message,
-    }); 
+    });
   }
-}
+};
 
 export default handler;
