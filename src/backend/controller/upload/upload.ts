e4b5978ed2ef0108c7543e90d88c52fcd3e8559f
Tapabrata Dey <tapabratadey02@gmail.com>
2022-03-01 16:45:16 -0500

    Update upload.ts 1. Refactoring code 2. Creating an unique key for s3 bucket

--- src/backend/controller/upload/upload.ts
+++ src/backend/controller/upload/upload.ts
@@ -1,32 +1,18 @@
 import UploadService from '@backend/services/db/upload/upload_db';
-import { NotFoundError } from 'helpers/ErrorHandling/errors'
-import chalk from 'chalk';
+import { NotFoundError } from 'helpers/ErrorHandling/errors';
+import { Request } from '@backend/middleware/verify_request';
 
 const UploadController = {
-  create: async (req, res) => {
-      try {
-        if (req) {
-            const {
-                key,
-                Bucket,
-                ContentType
-            } = req.body;
-            const uploadConfig = await UploadService.create(req.body, res);
-            return uploadConfig;
-        } else {
-            throw new NotFoundError('Upload configuration not provided.')
-        }
-      } catch (error) {
-        console.log(
-            chalk.red.bold(error.name + '@backend/controller/upload/upload.ts on Line 21'),
-            error.message
-          );
-        res.status(error.code).json({
-            status: error.status,
-            message: error.message,
-        });
-      }
-  }
+  create: async (req: Request) => {
+    const {
+      user: { uid },
+    } = req;
+    const key = `${uid}/${Date.now()}.jpeg`;
+    const uploadConfig = await UploadService.create(key);
+    if (!uploadConfig)
+      throw new NotFoundError('Upload configuration not provided.');
+    return uploadConfig;
+  },
 };
 
 export default UploadController;
