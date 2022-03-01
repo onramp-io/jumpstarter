140ba5fba3e2b0886ca58221c63fe2ea02e9286c
Tapabrata Dey <tapabratadey02@gmail.com>
2022-03-01 16:52:24 -0500

    Removing duplicate class

--- src/helpers/ErrorHandling/errors.ts
+++ src/helpers/ErrorHandling/errors.ts
@@ -4,10 +4,10 @@ export class DatabaseError extends Error {
 
   constructor(message: string) {
     super(message);
-    this.name = "DatabaseError";
+    this.name = 'DatabaseError';
     this.message = message;
     this.code = 500;
-    this.status = "Internal Server Error";
+    this.status = 'Internal Server Error';
   }
 
   toJSON() {
@@ -44,29 +44,7 @@ export class QueryError extends Error {
 export class UserFacingError extends Error {
   constructor(message: string) {
     super(message);
-    this.name = "UserFacingError";
-  }
-}
-
-export class BadRequestError extends UserFacingError {
-  code: number;
-  status: string;
-
-  constructor(message: string) {
-    super(message);
-    this.name = "BadRequestError";
-    this.message = message;
-    this.code = 400;
-    this.status = "Bad Request";
-  }
-
-  toJSON() {
-    return {
-      name: this.name,
-      code: this.code,
-      status: this.status,
-      message: this.message,
-    };
+    this.name = 'UserFacingError';
   }
 }
 
@@ -98,10 +76,10 @@ export class NotFoundError extends UserFacingError {
 
   constructor(message: string) {
     super(message);
-    this.name = "NotFoundError";
+    this.name = 'NotFoundError';
     this.message = message;
     this.code = 404;
-    this.status = "Not Found";
+    this.status = 'Not Found';
   }
 
   toJSON() {
@@ -120,10 +98,10 @@ export class AuthorizationError extends UserFacingError {
 
   constructor(message: string) {
     super(message);
-    this.name = "UserNotAuthorizedError";
+    this.name = 'UserNotAuthorizedError';
     this.message = message;
     this.code = 401;
-    this.status = "Unauthorized";
+    this.status = 'Unauthorized';
   }
 
   toJSON() {
@@ -142,10 +120,10 @@ export class MethodNotAllowedError extends UserFacingError {
 
   constructor(message: string) {
     super(message);
-    this.name = "MethodNotAllowedError";
+    this.name = 'MethodNotAllowedError';
     this.message = message;
     this.code = 405;
-    this.status = "Method Not Allowed";
+    this.status = 'Method Not Allowed';
   }
 
   toJSON() {
