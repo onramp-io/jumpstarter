d9775bebc3571ba221d1b6c745162ee9e8487925
Tapabrata Dey <tapabratadey02@gmail.com>
2022-03-01 16:59:53 -0500

    Update edit.tsx 1. Refactoring code 2. Rendering User avatar if not default 3. On submit of new image, get signed S3 URL -> upload to the signed S3 URL -> save key to db -> render

--- src/pages/app/profile/edit.tsx
+++ src/pages/app/profile/edit.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from "next";
-import styles from "../../../styles/EditUser.module.css";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import type { NextPage } from 'next';
+import styles from '../../../styles/EditUser.module.css';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 import {
   Heading,
@@ -12,9 +12,7 @@ import {
   Avatar,
   FileInput,
   Text,
-<<<<<<< HEAD
 } from 'grommet';
-// import axios from '../../../axios/instance';
 import axios from 'axios';
 import { useAuth } from '@frontend/context/AuthProvider';
 import { deleteUser, getAuth } from 'firebase/auth';
@@ -26,60 +24,46 @@ const EditProfile: NextPage = () => {
   const [bioValue, setBio] = useState('');
   const [avatarImg, setAvatar] = useState<File>(null);
   const [errorMessage, setError] = useState('');
-=======
-} from "grommet";
-import axios from "../../../axios/instance";
-import { useAuth, useUserDispatch } from "@frontend/context/AuthProvider";
-import { deleteUser, getAuth } from "firebase/auth";
-import { Alert, AlertTitle } from "@mui/material";
-
-const EditProfile: NextPage = () => {
-  const [fName, setFName] = useState("");
-  const [lName, setLName] = useState("");
-  const [bioValue, setBio] = useState("");
-  const [errorMessage, setError] = useState("");
->>>>>>> main
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const { firstName, lastName, bio, avatar, accessToken } = useAuth();
 
   const router = useRouter();
 
-  const onChangeFName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFName(e.target.value);
-  };
-
-  const onChangeLName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLName(e.target.value);
-  };
-
-  const onChangeBio = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setBio(e.target.value);
-  };
-
   const handleSubmit = async () => {
     setIsSubmitting(true);
     try {
-      const body = {
-        firstName: fName,
-        lastName: lName,
-        bio: bioValue,
-      };
-
       const headers = {
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
       };
+      // 1. Get the AWS S3 signed url
+      const uploadConfig = await axios.get('/api/upload', headers);
 
-      const updateUser = await axios.put('/api/users/update', body, headers);
-      
-      await axios.put('/api/users/updateAvatar', avatarImg, {
-        headers: {
-          'Content-type': avatarImg.type,
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
+      // 2. Upload the file to the signed url
+      const userAvatar = await axios.put(
+        uploadConfig.data.uploadConfig.url,
+        avatarImg,
+        {
+          headers: {
+            'Content-type': avatarImg.type,
+          },
+        }
+      );
+
+      // 3. Update the user profile
+      const body = {
+        firstName: fName,
+        lastName: lName,
+        bio: bioValue,
+        avatarImgUrl: uploadConfig.data.uploadConfig.randomKey,
+      };
+      const updateUserProfile = await axios.put(
+        '/api/users/update',
+        body,
+        headers
+      );
 
       router.push('/app/profile');
     } catch (error) {
@@ -94,7 +78,7 @@ const EditProfile: NextPage = () => {
       const user = auth.currentUser;
       deleteUser(user).then(async () => {
         const res = await axios.delete(`/users/delete`);
-        router.push("/");
+        router.push('/');
       });
     } catch (error) {
       setError(error.message);
@@ -108,18 +92,30 @@ const EditProfile: NextPage = () => {
         <Box className={styles.container}>
           <Box className={styles.avatar}>
             <Box direction="row" gap="small">
-              <Avatar
-                src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80"
-                size="3xl"
-                className={styles.avatarImg}
-              />
+              {avatar ? (
+                <>
+                  <Avatar
+                    src={process.env.AWS_BUCKET_URL + avatar}
+                    size="3xl"
+                    className={styles.avatarImg}
+                  />
+                </>
+              ) : (
+                <>
+                  <Avatar
+                    src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80"
+                    size="3xl"
+                    className={styles.avatarImg}
+                  />
+                </>
+              )}
             </Box>
             <FileInput
               name="image"
               type="File"
               placeholder="Upload a profile picture"
-              onChange={(event) => {
-                setAvatar(event.target.files[0]);
+              onChange={(e) => {
+                setAvatar(e.target.files[0]);
               }}
             />
           </Box>
@@ -132,7 +128,7 @@ const EditProfile: NextPage = () => {
                 placeholder={firstName}
                 value={fName}
                 onChange={(e) => {
-                  onChangeFName(e);
+                  setFName(e.target.value);
                 }}
               />
             </Box>
@@ -144,7 +140,7 @@ const EditProfile: NextPage = () => {
                 placeholder={lastName}
                 value={lName}
                 onChange={(e) => {
-                  onChangeLName(e);
+                  setLName(e.target.value);
                 }}
               />
             </Box>
@@ -156,7 +152,7 @@ const EditProfile: NextPage = () => {
                 placeholder={bio}
                 value={bioValue}
                 onChange={(e) => {
-                  onChangeBio(e);
+                  setBio(e.target.value);
                 }}
                 className={styles.bio}
               />
@@ -176,7 +172,7 @@ const EditProfile: NextPage = () => {
         <Text onClick={delUser} className={styles.deleteUser}>
           Delete Account
         </Text>
-        {errorMessage !== "" && (
+        {errorMessage !== '' && (
           <Alert severity="error">
             <AlertTitle>{errorMessage}</AlertTitle>
           </Alert>
