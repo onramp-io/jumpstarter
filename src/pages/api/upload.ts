849acafcb38784ce98603ad70d93a8f840891c97
Tapabrata Dey <tapabratadey02@gmail.com>
2022-03-01 16:53:39 -0500

    Update upload.ts handler 1. Adding middleware to image upload handler 2. Updating response data 3. Changing method to GET

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,32 +1,35 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { MethodNotAllowedError } from "helpers/ErrorHandling/errors";
-import UploadController from "@backend/controller/upload/upload";
-import RequestMethod from "@backend/common/RequestMethod";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { MethodNotAllowedError } from 'helpers/ErrorHandling/errors';
+import UploadController from '@backend/controller/upload/upload';
+import RequestMethod from '@backend/common/RequestMethod';
 import chalk from 'chalk';
+import { Success } from 'helpers/ErrorHandling/success';
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-  ) {
-      try {
-          switch (req.method) {
-            case RequestMethod.POST:
-                console.log(`you're at the /pages/api/upload/index NextApiHandler's POST method!`)
-                 const uploadConfig = await UploadController.create(req, res);
-                 res.send(uploadConfig);
-                break;
-            default: 
-                throw new MethodNotAllowedError('Method not found');
-          }
-      } catch (error) {
-        console.log(
-            chalk.red.bold(error.name + '@api/upload.ts on Line 23'),
-            error.message
-          );
-          res.status(error.code).json({
-            status: error.status,
-            message: error.message,
-          });
-      }
+import { Request, verifyRequest } from '@backend/middleware/verify_request';
+const handler = async (req: Request, res: NextApiResponse) => {
+  try {
+    switch (req.method) {
+      case RequestMethod.GET:
+        const uploadConfig = await UploadController.create(req);
+        res.status(Success.code).json({
+          status: Success.status,
+          message: Success.message,
+          uploadConfig,
+        });
+        break;
+      default:
+        throw new MethodNotAllowedError('Method not found');
+    }
+  } catch (error) {
+    console.log(
+      chalk.red.bold(error.name + '@api/upload.ts on Line 23'),
+      error.message
+    );
+    res.status(error.code).json({
+      status: error.status,
+      message: error.message,
+    });
   }
+};
 
+export default verifyRequest(handler);
