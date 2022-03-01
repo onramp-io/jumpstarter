f0344f4e07eeada5cb0ec2e1881363b155492ba0
Tapabrata Dey <tapabratadey02@gmail.com>
2022-03-01 12:46:17 -0500

    New route handler to upload an avatar

---
+++ src/pages/api/users/updateAvatar.ts
@@ -0,0 +1,42 @@
+import { verifyRequest } from '@backend/middleware/verify_request';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { UserController } from '../../../backend/controller/user/user';
+
+import chalk from 'chalk';
+import {
+  DatabaseError,
+  MethodNotAllowedError,
+  NotFoundError,
+} from 'helpers/ErrorHandling/errors';
+import { Success } from 'helpers/ErrorHandling/success';
+
+interface Request extends NextApiRequest {
+  user: any;
+}
+
+const handler = async (req: Request, res: NextApiResponse) => {
+  try {
+    switch (req.method) {
+      case 'PUT':
+        await UserController.putAvatar(req);
+        res.status(Success.code).json({
+          status: Success.status,
+          message: Success.message,
+        });
+        break;
+      default:
+        throw new MethodNotAllowedError('Method not found');
+    }
+  } catch (error) {
+    console.log(
+      chalk.red.bold(error.name + '@user/update.ts on Line 32'),
+      error.message
+    );
+    res.status(error.code).json({
+      status: error.status,
+      message: error.message,
+    });
+  }
+};
+
+export default verifyRequest(handler);
