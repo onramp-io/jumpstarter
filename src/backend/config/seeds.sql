0556f08d57e0ffcaf6bcca9498edc6aa86881af3
Tapabrata Dey <tapabratadey02@gmail.com>
2022-03-02 09:30:07 -0500

    New data

--- src/backend/config/seeds.sql
+++ src/backend/config/seeds.sql
@@ -52,3 +52,39 @@ INSERT INTO public.category ("category", "picture")
     VALUES ('Games', 'none');
 INSERT INTO public.category ("category", "picture")
     VALUES ('Music', 'none');
+
+
+
+UPDATE public.user SET "investedAmt" = 5000, "balance" = 10000 WHERE id = 34
+UPDATE public.user SET "investedAmt" = 5000, "balance" = 10000 WHERE id = 35
+UPDATE public.user SET "investedAmt" = 5000, "balance" = 10000 WHERE id = 36
+UPDATE public.user SET "investedAmt" = 5000, "balance" = 10000 WHERE id = 37
+
+
+INSERT INTO public.project ("title", "category", "description", "fundTiers", "currFundGoal", "launchDate", "likesAmt", "userId")
+    VALUES ('MarsLink', 'Design & Tech', 'Marslink provides high-speed, low-latency broadband internet across Mars. Currently serving first-basis customers in Holden Crater.', Array[0,10,20,30], 20000, '2004-10-19 10:23:54', 100, 35);
+
+INSERT INTO public.project ("title", "category", "description", "fundTiers", "currFundGoal", "launchDate", "likesAmt", "userId")
+    VALUES ('ARK Invest 365', 'Design & Tech', 'Traveling to Mars? But still want to keep investing? Introducing ARK Invest 365, helps you invest during your interplanetary travles', Array[0,10,20,30], 20000, '2004-10-19 10:23:54', 100, 36);
+
+INSERT INTO public.project ("title", "category", "description", "fundTiers", "currFundGoal", "launchDate", "likesAmt", "userId")
+    VALUES ('Bitcoin Mars', 'Design & Tech', 'Bitcoin is an innovative payment network and a new kind of money, for Mars.', Array[0,10,20,30], 20000, '2004-10-19 10:23:54', 100, 37);
+
+
+INSERT INTO public.investment ("fundAmt", "userId", "projectId")
+    VALUES (5000, 34, 70);
+
+INSERT INTO public.investment ("fundAmt", "userId", "projectId")
+    VALUES (5000, 35, 70);
+
+INSERT INTO public.investment ("fundAmt", "userId", "projectId")
+    VALUES (5000, 36, 70);
+
+INSERT INTO public.comment ("comment", "userId", "projectId")
+    VALUES ('What a project! As Martian crypto grows, at Blend we will expand to support our friendly Martians with Bitcoin Mars. Wanna catch up for a quick chat, Satoshi?', 34, 70);
+
+INSERT INTO public.comment ("comment", "userId", "projectId")
+    VALUES ('Thanks, Satoshi! As the king of Mars, I hereby declare Bitcoin Mars to be the official currency of Mars. Satoshi, can we name it Marscoin?', 35, 70);
+
+INSERT INTO public.comment ("comment", "userId", "projectId")
+    VALUES ('Incredible!! Bitcoin Mars will help our interplanetary travelers invest so easily!', 36, 70);
