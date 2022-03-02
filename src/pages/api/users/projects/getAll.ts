42ea4f84e033cdd54b8680d8902b9c38c8b9a1cf
Tapabrata Dey <tapabratadey02@gmail.com>
2022-03-01 20:23:40 -0500

    New Route: /users/projects/getAll

---
+++ src/pages/api/users/projects/getAll.ts
@@ -0,0 +1,43 @@
+import { verifyRequest } from '@backend/middleware/verify_request';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { UserController } from '../../../../backend/controller/user/user';
+
+import chalk from 'chalk';
+import {
+  DatabaseError,
+  MethodNotAllowedError,
+  NotFoundError,
+} from 'helpers/ErrorHandling/errors';
+import { Success } from 'helpers/ErrorHandling/success';
+
+interface Request extends NextApiRequest {
+  user: any;
+}
+
+const handler = async (req: Request, res: NextApiResponse) => {
+  try {
+    switch (req.method) {
+      case 'GET':
+        const userProjects = await UserController.getUserProject(req);
+        res.status(Success.code).json({
+          status: Success.status,
+          message: Success.message,
+          userProjects,
+        });
+        break;
+      default:
+        throw new MethodNotAllowedError('Method not found');
+    }
+  } catch (error) {
+    console.log(
+      chalk.red.bold(error.name + '@users/projects/getAll.ts on Line 32'),
+      error.message
+    );
+    res.status(error.code).json({
+      status: error.status,
+      message: error.message,
+    });
+  }
+};
+
+export default verifyRequest(handler);
