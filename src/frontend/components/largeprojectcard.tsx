898b33c27803bd6b31862fb9c9dfda6d7d79ece6
Marri Kang <mkang@bl-mkang-w3704q2tjn.lan>
2022-03-02 13:34:38 -0500

    Pull proper information fields into large project card component

--- src/frontend/components/largeprojectcard.tsx
+++ src/frontend/components/largeprojectcard.tsx
@@ -10,9 +10,10 @@ type projectType = {
   title: string,
   category: string,
   description: string,
-  currFundGoal: number,
-  fundRaised: number
+  fundTiers: number[],
+  fundRaised: number,
   launchDate: Date,
+  pictures: string[]
 }
 
 interface LargeProjectCardProps {
@@ -23,7 +24,7 @@ const LargeProjectCard: NextPage<LargeProjectCardProps> = ({projectData}): JSX.E
   const [state, setState] = useState(projectData);
   useEffect(() => {
     const getUser = async (id) => {
-      const projectInfo = await axios.get(`/api/projects//project/${id}`);
+      const projectInfo = await axios.get(`/api/projects/project/${id}`);
 
       setState({...state, user_name: projectInfo.data.data.firstName + " " + projectInfo.data.data.lastName});
     }
@@ -31,7 +32,6 @@ const LargeProjectCard: NextPage<LargeProjectCardProps> = ({projectData}): JSX.E
     getUser(projectData.id);
   }, [])
 
-  console.log(projectData.fundRaised)
   const router = useRouter();
   
   const goToProject = async (event: MouseEvent) => {
@@ -57,22 +57,22 @@ const LargeProjectCard: NextPage<LargeProjectCardProps> = ({projectData}): JSX.E
         width="large"
         height="small"
       >
-        <Image fit="cover" src="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" />
+        <Image fit="cover" src={state.pictures ? state.pictures[0] : "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"} />
       </Box>
 
       <Box direction="column" gap="small" margin={{vertical: "medium", horizontal: "medium"}}>
-        <Anchor href="#" label={ projectData.category.toUpperCase() } size="small"/>
-        <Text weight="bold" size="large">{ projectData.title }</Text>
+        <Anchor href="#" label={ state.category.toUpperCase() } size="small"/>
+        <Text weight="bold" size="large">{ state.title }</Text>
         <Box max-height="min-content">
-          <Text>{ projectData.description }</Text>
+          <Text>{ state.description }</Text>
         </Box>
         <Box margin={{top: "small", bottom: "medium"}}>
           <Text size="small">Created by</Text>
           <Anchor href="#" label={ state.user_name } />
         </Box>
-        <Meter type="bar" value={ projectData.fundRaised } max={ projectData.currFundGoal }/>
+        <Meter type="bar" value={ state.fundRaised } max={ state.fundTiers[3] }/>
         <Box margin={{bottom: "small"}}>
-          <Text><strong>${projectData.fundRaised.toLocaleString()}</strong> <small>raised out of ${projectData.currFundGoal[-1].toLocaleString()} </small></Text>
+          <Text><strong>${state.fundRaised ? state.fundRaised.toLocaleString() : 0}</strong> <small>raised out of ${state.fundTiers[3].toLocaleString()} </small></Text>
           <Text size="small">12 days left</Text>
         </Box>
       </Box>
