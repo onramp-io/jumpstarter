459219d94fe1dffa01399d52854f3bb9b93ffdf0
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2022-03-04 16:42:44 -0800

    Fix Preferences Page UI
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- src/frontend/components/preferencesComponent.tsx
+++ src/frontend/components/preferencesComponent.tsx
@@ -2,7 +2,7 @@ import { Box, Button, Heading } from "grommet";
 
 import { motion } from "framer-motion";
 import styles from "../../styles/Preference.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { AlertTitle, Alert } from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress";
@@ -29,6 +29,10 @@ const UserPreferences: React.FC<UserPreferencesProps> = ({ categories }) => {
     }
   };
 
+  useEffect(() => {
+    console.log(selectedCategories);
+  }, [selectedCategories]);
+
   const submitPreferences = async () => {
     setIsSubmitting(true);
     try {
@@ -43,51 +47,78 @@ const UserPreferences: React.FC<UserPreferencesProps> = ({ categories }) => {
     setIsSubmitting(false);
   };
 
+  const tick = 0.1;
+  const staggerTotalDuration = 6 * tick;
+  const pulse = {
+    scale: [1, 0.9, 1.1, 1],
+    transition: {
+      duration: tick * 40,
+      delay: staggerTotalDuration,
+      repeat: Infinity,
+      repeatDelay: tick * 10,
+    },
+  };
+
   return (
     <>
-      <Box className={styles.wrapper}>
-        <Heading className={styles.header}>Preferences</Heading>
-        <Box className={styles.container}>
-          {categories &&
-            categories.map((item) => (
-              <motion.div key={item.id} whileHover={Animations.scaleOnHover}>
-                <Box
-                  elevation="medium"
-                  margin={{
-                    bottom: "large",
-                    right: "medium",
-                  }}
-                  direction="column"
-                  height="min(300px)"
-                  width="max(300px)"
-                  justify="center"
-                  pad="medium"
-                  align="center"
+      <Box
+        className="preferencesComponent_container"
+        align="center"
+        height="100vh"
+      >
+        <Box align="center" width="80vw" direction="column">
+          <Heading className={styles.header}>Preferences</Heading>
+          <Box
+            className={`${styles.container} preferencesComponent_cardBox`}
+            height="min-content"
+            margin="large"
+          >
+            {categories &&
+              categories.map((item) => (
+                <motion.div
+                  key={item.id}
+                  whileHover={Animations.scaleOnHover}
+                  whileTap={pulse}
                   onClick={() => addPreference(item.category)}
                 >
-                  <div>{item.category}</div>
-                </Box>
-              </motion.div>
-            ))}
-          {isSubmitting ? (
-            <>
-              <CircularProgress />
-            </>
-          ) : (
-            <>
-              <Button
-                primary
-                label="Submit my preferences"
-                onClick={submitPreferences}
-                disabled={isSubmitting}
-              />
-              {errorMessage !== "" && (
-                <Alert severity="error">
-                  <AlertTitle>{errorMessage}</AlertTitle>
-                </Alert>
-              )}
-            </>
-          )}
+                  <Box
+                    className="card"
+                    elevation="medium"
+                    margin="medium"
+                    direction="column"
+                    height="min(300px)"
+                    width="max(300px)"
+                    justify="center"
+                    pad="medium"
+                    align="center"
+                  >
+                    <Box className="preferencesComponent_itemCategory card">
+                      {item.category}
+                    </Box>
+                  </Box>
+                </motion.div>
+              ))}
+            {isSubmitting ? (
+              <>
+                <CircularProgress />
+              </>
+            ) : (
+              <>
+                <Button
+                  margin={{ top: "large" }}
+                  primary
+                  label="Submit my preferences"
+                  onClick={submitPreferences}
+                  disabled={isSubmitting}
+                />
+                {errorMessage !== "" && (
+                  <Alert severity="error">
+                    <AlertTitle>{errorMessage}</AlertTitle>
+                  </Alert>
+                )}
+              </>
+            )}
+          </Box>
         </Box>
       </Box>
     </>
