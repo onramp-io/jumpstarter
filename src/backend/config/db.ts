6a1097ae0da5a5260f2b4f5923232d00d03411e8
Tapabrata Dey <tapabratadey02@gmail.com>
2022-03-04 20:02:32 -0500

    Refactor db.ts 1. Removing commented code Co-authored-by: Pranitha Maganty <pmaganty@users.noreply.github.com>

--- src/backend/config/db.ts
+++ src/backend/config/db.ts
@@ -6,39 +6,11 @@ import { Comment } from '../entities/Comment';
 import { Like } from '../entities/Like';
 import { Category } from '../entities/Category';
 
-import chalk from 'chalk';
-/**
- * 
-//create typeorm database connection using ormconfig.json file
-const connection = async () => {
-  try {
-    const staleConnection = getConnection();
-    await staleConnection.close();
-  } catch (error) {}
-
-  const temp = await createConnection({
-    type: process.env.DB_TYPE as any,
-    url: process.env.DB_CONNECTION_STRING,
-    synchronize: true,
-    logging: true,
-    cache: {
-      duration: 30000, // 30 seconds
-    },
-    entities: [User, Project, Investment, Comment, Like, Category],
-  });
-  if (getConnection().isConnected) {
-    return getConnection();
-  }
-};
- * 
- */
-
 let connectionReadyPromise: Promise<Connection> | null = null;
 
 const connection = async () => {
   if (!connectionReadyPromise) {
     connectionReadyPromise = (async () => {
-      // clean up old connection that references outdated hot-reload classes
       try {
         const staleConnection = getConnection();
         await staleConnection.close();
@@ -61,7 +33,6 @@ const connection = async () => {
     })();
   }
 
-  // return getConnection();
   return connectionReadyPromise;
 };
 
