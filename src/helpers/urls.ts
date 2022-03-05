f026b42ef52936a6a5bf4d2e6c91980ef8c0531e
Tapabrata Dey <tapabratadey02@gmail.com>
2022-03-04 21:12:25 -0500

    Update urls.ts - Adding URL constants Co-authored-by: Pranitha Maganty <pmaganty@users.noreply.github.com>

--- src/helpers/urls.ts
+++ src/helpers/urls.ts
@@ -7,20 +7,52 @@ const projectView = '/api/projects/views/';
 const projects = '/api/projects/';
 const edit = '/app/edit/';
 const trending = '/api/projects/sort/TRENDING';
+const newest = '/api/projects/sort/NEWEST';
 const likes = '/likes/';
 const checkout = '/app/checkout/';
 const loginRedirect = '/login';
 const projectRedirect = '/app/project/';
-
+const recommend = '/users/recommend';
+const upload = '/api/upload';
+const categories = '/api/categories';
+const deleteUser = '/users/delete';
+const userUpdate = '/api/users/update';
+const userInvestments = '/investments/get';
+const payout = '/users/payout';
+const getAllUserProjects = '/users/projects/getAll';
+const SSRURL = 'http://localhost:3000';
 
 const urlCheck = async (router, user) => {
-    if (!router && !user) {
-        return false;
-    } else {
-        return true;
-    }
-}
-    
-export default { addInvestment, getUser, profileRedirect, 
-    comments, projectView, projects, getUserApi, edit, urlCheck, 
-    trending, likes, checkout, loginRedirect, projectRedirect }
+  if (!router && !user) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+export default {
+  addInvestment,
+  getUser,
+  profileRedirect,
+  comments,
+  projectView,
+  projects,
+  getUserApi,
+  edit,
+  urlCheck,
+  trending,
+  likes,
+  checkout,
+  loginRedirect,
+  projectRedirect,
+  newest,
+  upload,
+  recommend,
+  categories,
+  deleteUser,
+  userUpdate,
+  userInvestments,
+  payout,
+  getAllUserProjects,
+  SSRURL,
+};
